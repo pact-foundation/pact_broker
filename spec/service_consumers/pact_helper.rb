3ed4029bbb28a2351cbc18d3b6298021a831aa15
Beth <beth@bethesque.com>
2013-10-25 11:28:57 +1100

    Have to run this with RACK_ENV=test rake db:migrate pact:verify:dev

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -11,20 +11,20 @@ Sequel.extension :migration
 RSpec.configure do | config |
   config.before :suite do
 
-    puts "RUNNING DB SETUP"
+    puts "BLAH"
     raise "Wrong environment!!! Don't run this script!! ENV['RACK_ENV'] is #{ENV['RACK_ENV']} and RACK_ENV is #{RACK_ENV}" if ENV['RACK_ENV'] != 'test' || RACK_ENV != 'test'
-
-    db_file = File.expand_path File.join(File.dirname(__FILE__), '../../tmp/pact_broker_database_test.sqlite3')
-    puts "DB FILE IS #{db_file} #{RACK_ENV}"
-    FileUtils.rm_rf db_file
-    sleep 1
-    Sequel::Migrator.run(DB::PACT_BROKER_DB, "db/migrations")
-    sleep 1
+    begin
+      db_file = File.expand_path File.join(File.dirname(__FILE__), '../../tmp/pact_broker_database_test.sqlite3')
+      FileUtils.rm_rf db_file
+      FileUtils.touch db_file
+      Sequel::Migrator.run(DB::PACT_BROKER_DB, "./db/migrations")
+    rescue StandardError => e
+      puts e
+    end
   end
 
-  config.before :each, :pact => :verify do
-    sleep 1
-    DB::PACT_BROKER_DB[:pacticipant].truncate
+  config.before :each do
+    DB::PACT_BROKER_DB[:pacticipants].truncate
   end
 
 end
