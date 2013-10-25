8280720ab749de41dd7ad4439d6038162a78aa81
Beth <beth@bethesque.com>
2013-10-25 11:20:13 +1100

    Trying to fix I/O error

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -15,9 +15,16 @@ RSpec.configure do | config |
     raise "Wrong environment!!! Don't run this script!! ENV['RACK_ENV'] is #{ENV['RACK_ENV']} and RACK_ENV is #{RACK_ENV}" if ENV['RACK_ENV'] != 'test' || RACK_ENV != 'test'
 
     db_file = File.expand_path File.join(File.dirname(__FILE__), '../../tmp/pact_broker_database_test.sqlite3')
-    puts "DB FILE IS #{db_file}"
+    puts "DB FILE IS #{db_file} #{RACK_ENV}"
     FileUtils.rm_rf db_file
+    sleep 1
     Sequel::Migrator.run(DB::PACT_BROKER_DB, "db/migrations")
+    sleep 1
+  end
+
+  config.before :each, :pact => :verify do
+    sleep 1
+    DB::PACT_BROKER_DB[:pacticipant].truncate
   end
 
 end
