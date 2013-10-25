26226f62d6a14f0b8305e94452591af37580a78c
Beth <beth@bethesque.com>
2013-10-25 16:20:06 +1100

    Updated constraints in database scripts.

--- db/migrations/02_create_pacts_table.rb
+++ db/migrations/02_create_pacts_table.rb
@@ -3,7 +3,9 @@ Sequel.migration do
     create_table(:pacts) do
       primary_key :id
       String :json_content
-      foreign_key :provider_id, :pacticipants
+      foreign_key :version_id, :versions, null: false
+      foreign_key :provider_id, :pacticipants, null: false
+      index [:version_id, :provider_id], :unique=>true
     end
   end
 end
--- db/migrations/03_create_versions_table.rb
+++ db/migrations/03_create_versions_table.rb
@@ -3,9 +3,10 @@ Sequel.migration do
   change do
     create_table(:versions) do
       primary_key :id
-      String :number, :unique => true
+      String :number
       String :repository_ref
-      foreign_key :pacticipant_id, :pacticipants
+      foreign_key :pacticipant_id, :pacticipants, :null=>false
+      index [:pacticipant_id, :number], :unique=>true
     end
   end
 
