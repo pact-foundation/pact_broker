3cd9c60383913ace0a44ad1e24156911f316dbdf
Beth <beth@bethesque.com>
2014-05-09 09:52:52 +1000

    Cleaned up cluster splitting code

--- bethtest.rb
+++ bethtest.rb
@@ -24,42 +24,39 @@ def unique_nodes links
   links.collect(&:to_a).flatten.uniq.sort
 end
 
-def nodes_connected_to_node node
-  unique_nodes @links.select{|l|l.include?(node)}
+def nodes_connected_to_node node, links
+  unique_nodes links.select{|l|l.include?(node)}
 end
 
-def nodes_connected_to_nodes nodes
-  nodes.collect{ | node | nodes_connected_to_node(node) }.flatten & @nodes
+def nodes_connected_to_nodes_within_pool nodes, links, node_pool
+  nodes.collect{ | node | nodes_connected_to_node(node, links) }.flatten & node_pool
 end
 
-def remove_nodes_from_pool nodes
-  nodes.each do | node |
-    @nodes.delete node
+def split_into_clusters links
+  node_pool =  unique_nodes links
+  groups = []
+
+  while node_pool.any?
+    group = []
+    groups << group
+    connected_nodes = [node_pool.first]
+
+    while connected_nodes.any?
+      group.concat(connected_nodes)
+      node_pool = node_pool - connected_nodes
+      connected_nodes = nodes_connected_to_nodes_within_pool connected_nodes, links, node_pool
+    end
   end
+
+  groups
 end
 
-
-@links = [Link.new('A', 'B'), Link.new('A', 'C'), Link.new('C', 'D'), Link.new('D', 'E'), Link.new('E','A'),
+links = [Link.new('A', 'B'), Link.new('A', 'C'), Link.new('C', 'D'), Link.new('D', 'E'), Link.new('E','A'),
   Link.new('Y', 'Z'), Link.new('X', 'Y'),
   Link.new('M', 'N'), Link.new('N', 'O'), Link.new('O', 'P'), Link.new('P','Q')]
 
-@nodes =  unique_nodes @links
 
-groups = []
-
-while @nodes.any?
-  group = []
-  groups << group
-  connections = [@nodes.first]
-  group.concat connections
-
-  while connections.any?
-    remove_nodes_from_pool connections
-    connections = nodes_connected_to_nodes connections
-    group.concat(connections)
-  end
-
-end
+groups = split_into_clusters links
 
 puts groups.collect{ | group| "group = #{group.join(" ")}"}
 
