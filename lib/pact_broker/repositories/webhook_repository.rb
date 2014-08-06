4f1666b38b476a8b2ac39f9ff7cef995a5f977a3
Beth <beth@bethesque.com>
2014-08-07 07:57:53 +1000

    Refactoring to still use Sequel model with decoupled domain model using repository pattern

--- lib/pact_broker/repositories/webhook_repository.rb
+++ lib/pact_broker/repositories/webhook_repository.rb
@@ -4,6 +4,7 @@ require 'pact_broker/db'
 
 module PactBroker
   module Repositories
+
     class WebhookRepository
 
       # Experimenting with decoupling the model from the database representation.
@@ -12,56 +13,83 @@ module PactBroker
       include Repositories
 
       def create webhook, consumer, provider
-        uuid = SecureRandom.urlsafe_base64
+        db_webhook = Webhook.from_model webhook
+        db_webhook.consumer_id = consumer.id
+        db_webhook.provider_id = provider.id
+        db_webhook.uuid = SecureRandom.urlsafe_base64
+        db_webhook.save
+
+        webhook.request.headers.each_pair do | name, value |
+          WebhookHeader.from_model(name, value, db_webhook.id).save
+        end
+
+        find_by_uuid db_webhook.uuid
+      end
+
+      def find_by_uuid uuid
+        db_webhook = Webhook.where(uuid: uuid).single_record
+        return nil if db_webhook.nil?
+        db_webhook.to_model
+      end
+
+    end
+
+    class Webhook < Sequel::Model
+
+      set_primary_key :id
+      associate(:many_to_one, :provider, :class => "PactBroker::Models::Pacticipant", :key => :provider_id, :primary_key => :id)
+      associate(:many_to_one, :consumer, :class => "PactBroker::Models::Pacticipant", :key => :consumer_id, :primary_key => :id)
+
+
+      def self.from_model webhook
         is_json_request_body = !(String === webhook.request.body || webhook.request.body.nil?) # Can't rely on people to set content type
-        webhook_id = PactBroker::DB.connection[:webhooks].
-          insert(
-            consumer_id: consumer.id,
-            provider_id: provider.id,
-            uuid: uuid,
+          new(
+            uuid: webhook.uuid,
             method: webhook.request.method,
             url: webhook.request.url,
             body: (is_json_request_body ? webhook.request.body.to_json : webhook.request.body),
             is_json_request_body: is_json_request_body
           )
-
-        webhook.request.headers.each_pair do | name, value |
-          PactBroker::DB.connection[:webhook_headers].insert(name: name, value: value, webhook_id: webhook_id)
-        end
-
-        find_by_uuid uuid
       end
 
-      def find_by_uuid uuid
-        webhook_record = PactBroker::DB.connection[:webhooks].where(uuid: uuid).single_record
-        return nil if webhook_record.nil?
-        create_webhook_model webhook_record
-      end
-
-      private
-
-      def create_webhook_model webhook_record
-        headers = find_webhook_headers webhook_record[:id]
-        body = webhook_body_from webhook_record
-        request_record = webhook_record.merge(headers: headers, body: body)
+      def to_model
         Models::Webhook.new(
-          uuid: webhook_record[:uuid],
-          consumer: pacticipant_repository.find_by_id(webhook_record[:consumer_id]),
-          provider: pacticipant_repository.find_by_id(webhook_record[:provider_id]),
-          request: Models::WebhookRequest.new(request_record))
+          uuid: uuid,
+          consumer: consumer,
+          provider: provider,
+          request: Models::WebhookRequest.new(request_attributes))
       end
 
-      def find_webhook_headers webhook_id
-        header_records = PactBroker::DB.connection[:webhook_headers].where(webhook_id: webhook_id)
-        header_records.each_with_object({}) { | header, hash | hash[header[:name]] = header[:value]}
+      def request_attributes
+        values.merge(headers: headers, body: parsed_body)
       end
 
-      def webhook_body_from webhook_record
-        if webhook_record[:body] && webhook_record[:is_json_request_body]
-           JSON.parse(webhook_record[:body])
-        else
-          webhook_record[:body]
+      def headers
+        WebhookHeader.where(webhook_id: id).all.each_with_object({}) do | header, hash |
+          hash[header[:name]] = header[:value]
         end
+      end
+
+      def parsed_body
+        if body && is_json_request_body
+           JSON.parse(body)
+        else
+          body
+        end
+      end
+
+    end
+
+    class WebhookHeader < Sequel::Model
+
+      associate(:many_to_one, :webhook, :class => "PactBroker::Repositories::Webhook", :key => :webhook_id, :primary_key => :id)
+
+      def self.from_model name, value, webhook_id
+        db_header = new
+        db_header.name = name
+        db_header.value = value
+        db_header.webhook_id = webhook_id
+        db_header
       end
 
     end
