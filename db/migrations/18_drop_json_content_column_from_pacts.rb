f01a0b639b3e389448da930f1d30ce62fe8e0544
Beth <beth@bethesque.com>
2014-11-18 22:54:30 +1100

    Dropping and recreating views. Otherwise Postgres blows up.

--- db/migrations/18_drop_json_content_column_from_pacts.rb
+++ db/migrations/18_drop_json_content_column_from_pacts.rb
@@ -3,6 +3,10 @@ require_relative 'migration_helper'
 
 Sequel.migration do
   change do
+    drop_view(:latest_pacts)
+    drop_view(:latest_pact_consumer_version_orders)
+    drop_view(:all_pacts)
+
     alter_table(:pacts) do
       drop_column(:json_content)
     end
--- db/migrations/21_recreate_latest_pacts_view.rb
+++ db/migrations/21_recreate_latest_pacts_view.rb
@@ -3,6 +3,12 @@ require_relative 'migration_helper'
 
 Sequel.migration do
   change do
+    create_or_replace_view(:latest_pact_consumer_version_orders,
+      "select provider_id, consumer_id, max(consumer_version_order) as latest_consumer_version_order
+      from all_pacts
+      group by provider_id, consumer_id"
+    )
+
     create_or_replace_view(:latest_pacts,
       "select ap.*
       from all_pacts ap
