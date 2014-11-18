300cc59d871b0348981048c60f39c3edd84cf2e5
Beth <beth@bethesque.com>
2014-11-18 19:08:17 +1100

    Removed json_content column from all_pacts view

--- db/migrations/15_create_pact_version_content.rb
+++ db/migrations/15_create_pact_version_content.rb
@@ -29,7 +29,7 @@ Sequel.migration do
       :c__id___consumer_id, :c__name___consumer_name,
       :cv__id___consumer_version_id, :cv__number___consumer_version_number, :cv__order___consumer_version_order,
       :p__id___provider_id, :p__name___provider_name,
-      :pvc__id___pact_version_content_id, :pvc__content___json_content, :pacts__created_at, :pacts__updated_at).
+      :pvc__id___pact_version_content_id, :pacts__created_at, :pacts__updated_at).
       join(:versions, {:id => :version_id}, {:table_alias => :cv, implicit_qualifier: :pacts}).
       join(:pacticipants, {:id => :pacticipant_id}, {:table_alias => :c, implicit_qualifier: :cv}).
       join(:pacticipants, {:id => :provider_id}, {:table_alias => :p, implicit_qualifier: :pacts}).
