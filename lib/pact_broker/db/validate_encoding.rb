56e2352d7b76633ae6b49599481edfcad0b282ab
Beth <beth@bethesque.com>
2014-11-20 07:54:07 +1100

    Added code to warn when connection encoding is not UTF8.

---
+++ lib/pact_broker/db/validate_encoding.rb
@@ -0,0 +1,22 @@
+require 'sequel'
+require 'pact_broker/messages'
+
+module PactBroker
+  module DB
+
+    class ConnectionConfigurationError < StandardError; end
+
+    class ValidateEncoding
+
+      extend PactBroker::Messages
+
+      def self.call connection
+        encoding = connection.opts[:encoding] || connection.opts['encoding']
+        unless encoding =~ /utf\-?8/i
+          raise ConnectionConfigurationError.new(message('errors.validation.connection_encoding_not_utf8', encoding: encoding.inspect))
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/lib/pact_broker/db/validate_encoding_spec.rb
@@ -0,0 +1,66 @@
+require 'pact_broker/db/validate_encoding'
+require 'pact_broker/db'
+
+module PactBroker
+  module DB
+
+    describe ValidateEncoding do
+
+      let(:opts) { {encoding: encoding} }
+      let(:connection) { double('connection', opts: opts)}
+
+      subject { ValidateEncoding.(connection) }
+
+      describe ".call" do
+        context "when encoding is UTF8" do
+          let(:encoding) { 'UTF8' }
+
+          it "does not raise an error" do
+            subject
+          end
+        end
+
+        context "when encoding is UTF8" do
+          let(:encoding) { 'utf8' }
+
+          it "does not raise an error" do
+            subject
+          end
+        end
+
+        context "when encoding is utf-8" do
+          let(:encoding) { 'utf-8' }
+
+          it "does not raise an error" do
+            subject
+          end
+        end
+
+        context "when encoding is utf-80" do
+          let(:encoding) { 'utf-80' }
+
+          it "does not raise an error, maybe it should, ah well" do
+            subject
+          end
+        end
+
+        context "when encoding is null" do
+          let(:encoding) { nil }
+
+          it "raises an error" do
+            expect{ subject }.to raise_error ConnectionConfigurationError, /The Sequel connection encoding \(nil\) is strongly recommended to be "utf8"/
+          end
+        end
+
+        context "when encoding is latin1" do
+          let(:encoding) { 'latin1' }
+
+          it "raises an error" do
+            expect{ subject }.to raise_error ConnectionConfigurationError, /The Sequel connection encoding \("latin1"\) is strongly recommended to be "utf8"/
+          end
+        end
+      end
+
+    end
+  end
+end
