a2413f4d4f74995f643aff5ea66e041b41e1aa30
Beth <beth@bethesque.com>
2014-12-03 13:16:06 +1100

    Stop Padrino trying to create a log file in the gem directory https://github.com/bethesque/pact_broker/issues/13

--- lib/pact_broker/ui.rb
+++ lib/pact_broker/ui.rb
@@ -1,2 +1,9 @@
+
+# Stop Padrino creating a log file, as it will try to create it in the gems directory
+# http://www.padrinorb.com/api/Padrino/Logger.html
+unless defined? PADRINO_LOGGER
+  PADRINO_LOGGER = { production: { log_level: :warn, stream: :null } }
+end
+
 require 'pact_broker/ui/controllers/relationships'
 require 'pact_broker/ui/controllers/groups'
