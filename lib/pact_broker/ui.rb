f468b2ca5087f053e75bd3be16e3b545bd4e4b1e
Beth <beth@bethesque.com>
2014-12-03 20:25:05 +1100

    Changed Padrino to log to stdout. :null creates a StringIO, don't want to hog memory.

--- lib/pact_broker/ui.rb
+++ lib/pact_broker/ui.rb
@@ -2,7 +2,12 @@
 # Stop Padrino creating a log file, as it will try to create it in the gems directory
 # http://www.padrinorb.com/api/Padrino/Logger.html
 unless defined? PADRINO_LOGGER
-  PADRINO_LOGGER = { production: { log_level: :warn, stream: :null } }
+  PADRINO_LOGGER = {
+    production:  { log_level: :error, stream: :stderr },
+    staging:     { log_level: :error, stream: :stderr },
+    test:        { log_level: :warn,  stream: :stdout },
+    development: { log_level: :warn,  stream: :stdout }
+  }
 end
 
 require 'pact_broker/ui/controllers/relationships'
