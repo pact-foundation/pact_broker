7d60992720808ab4e97699f9a9d4b99bf8f0e525
Bo Daley <bdaley@zipcar.com>
2016-11-30 19:13:31 -0500

    Revert "migration 17 now runs against sqlite3 backend"
    This reverts commit 090b836162e84cb311f59cf9389dc67b31c9ce2e.

--- db/migrations/17_migrate_json_content_to_pact_version_content_table.rb
+++ db/migrations/17_migrate_json_content_to_pact_version_content_table.rb
@@ -4,7 +4,7 @@ require_relative 'migration_helper'
 Sequel.migration do
   change do
     self[:pacts].each do | row |
-      sha = Digest::SHA1.hexdigest(row[:json_content].to_s)
+      sha = Digest::SHA1.hexdigest(row[:json_content])
       if self[:pact_version_contents].where(sha: sha).count == 0
         self[:pact_version_contents].insert(sha: sha, content: row[:json_content], created_at: row[:created_at], updated_at: row[:updated_at])
       end
