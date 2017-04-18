1d71336313245ff4a31c52b0e784600dbd80b151
Beth Skurrie <beth@bethesque.com>
2017-04-18 17:26:04 +1000

    Ensure pact content sha is unique in schema.

--- db/migrations/23_create_pact_contents_table.rb
+++ db/migrations/23_create_pact_contents_table.rb
@@ -4,7 +4,7 @@ Sequel.migration do
   up do
     create_table(:pact_contents, charset: 'utf8') do
       primary_key :id
-      String :sha, null: false, primary_key_constraint_name: 'pk_pact_version'
+      String :sha, null: false, unique: true, unique_constraint_name: 'unq_pvc_sha'
       String :content, type: PactBroker::MigrationHelper.large_text_type
       DateTime :created_at, null: false
     end
