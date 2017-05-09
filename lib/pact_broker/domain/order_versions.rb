251c878d0ce1594462d7cfc9b99cbb0621900627
Beth Skurrie <beth@bethesque.com>
2017-05-09 13:22:36 +1000

    Allow application versions to be ordered by creation date where no consistent orderable object can be extracted from the consumer application version.

--- lib/pact_broker/domain/order_versions.rb
+++ lib/pact_broker/domain/order_versions.rb
@@ -4,9 +4,17 @@ module PactBroker
   module Domain
     class OrderVersions
 
+      # TODO select for update
       def self.call pacticipant_id
-        orderable_versions = PactBroker::Domain::Version.where(:pacticipant_id => pacticipant_id).all.collect{| version | OrderableVersion.new(version) }
-        orderable_versions.sort.each_with_index{ | version, i | version.update_model(i) }
+        PactBroker::Domain::Version.db.transaction do
+          orderable_versions = PactBroker::Domain::Version.for_update.where(:pacticipant_id => pacticipant_id).order(:created_at, :id).collect{| version | OrderableVersion.new(version) }
+          ordered_versions = if PactBroker.configuration.order_versions_by_date
+            orderable_versions # already ordered in SQL
+          else
+            orderable_versions.sort
+          end
+          ordered_versions.each_with_index{ | version, i | version.update_model(i) }
+        end
       end
 
       class OrderableVersion
@@ -23,9 +31,11 @@ module PactBroker
         end
 
         def update_model new_order
-          # Sequel will only run the update if the column value has changed, so in 99% of
-          # cases, only one update will occur.
-          version_model.update(:order => new_order)
+          if version_model.order != new_order
+            # Avoid modifying the updated_at flag by doing a manual update
+            # In 99% of cases, this will only set the order of the most recent version
+            PactBroker::Domain::Version.where(id: version_model.id).update(order: new_order)
+          end
         end
       end
     end
--- spec/lib/pact_broker/domain/order_versions_spec.rb
+++ spec/lib/pact_broker/domain/order_versions_spec.rb
@@ -4,20 +4,40 @@ require 'pact_broker/domain/order_versions.rb'
 
 describe PactBroker::Domain::OrderVersions do
 
+  context "when order_versions_by_date is false (the default)" do
+    before do
+      ProviderStateBuilder.new
+        .create_condor
+        .create_condor_version('1.3.0')
+        .create_condor_version('1.5.0')
+        .create_condor_version('1.4.0')
+        .create_condor_version('1.6.0')
+    end
 
-  before do
-    ProviderStateBuilder.new
-      .create_condor
-      .create_condor_version('1.3.0')
-      .create_condor_version('1.5.0')
-      .create_condor_version('1.4.0')
+    let(:ordered_versions) { PactBroker::Domain::Version.order(:order).all.collect(&:number) }
+    let(:condor) { PactBroker::Domain::Pacticipant.where(name: 'Condor').single_record }
+
+    it "orders the versions so they can be loaded from the database in order" do
+      expect(ordered_versions).to eq(['1.3.0', '1.4.0', '1.5.0', '1.6.0'])
+    end
   end
 
-  let(:ordered_versions) { PactBroker::Domain::Version.order(:order).all.collect(&:number) }
-  let(:condor) { PactBroker::Domain::Pacticipant.where(name: 'Condor').single_record }
+  context "when order_versions_by_date is true (not recommended)" do
+    before do
+      allow(PactBroker.configuration).to receive(:order_versions_by_date).and_return(true)
+    end
+    let(:consumer) { ProviderStateBuilder.new.create_consumer.and_return(:consumer) }
+    let!(:version_1) { PactBroker::Domain::Version.create(pacticipant_id: consumer.id, number: '2', created_at: DateTime.new(2017)) }
+    let!(:version_2) { PactBroker::Domain::Version.create(pacticipant_id: consumer.id, number: '1', created_at: DateTime.new(2017)) }
+    let!(:version_3) { PactBroker::Domain::Version.create(pacticipant_id: consumer.id, number: '3', created_at: DateTime.new(2016)) }
+    let!(:version_4) { PactBroker::Domain::Version.create(pacticipant_id: consumer.id, number: '4', created_at: DateTime.new(2018)) }
 
-  it "orders the versions so they can be loaded from the database in order" do
-    expect(ordered_versions).to eq(['1.3.0','1.4.0', '1.5.0'])
+    let(:ordered_versions) { PactBroker::Domain::Version.order(:order).all.collect(&:number) }
+
+    it "orders by date, then id" do
+      expect(ordered_versions).to eq(['3', '2', '1', '4'])
+    end
+
   end
 
 end
