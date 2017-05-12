c724b185aae72ceeebbb494b8366dca188870127
Beth Skurrie <beth@bethesque.com>
2017-05-12 12:10:41 +1000

    Update migration 28 to new Sequel syntax

--- db/migrations/28_create_all_pact_publications.rb
+++ db/migrations/28_create_all_pact_publications.rb
@@ -2,11 +2,12 @@ Sequel.migration do
   up do
     # The denormalised pact publication details for each publication
     create_view(:all_pact_publications,
-      Sequel::Model.db[:pact_publications].select(:pact_publications__id,
-      :c__id___consumer_id, :c__name___consumer_name,
-      :cv__id___consumer_version_id, :cv__number___consumer_version_number, :cv__order___consumer_version_order,
-      :p__id___provider_id, :p__name___provider_name,
-      :pact_publications__revision_number, :pv__id___pact_version_id, :pv__sha___pact_version_sha, :pact_publications__created_at).
+      Sequel::Model.db[:pact_publications].select(
+      Sequel[:pact_publications][:id],
+      Sequel[:c][:id].as(:consumer_id), Sequel[:c][:name].as(:consumer_name),
+      Sequel[:cv][:id].as(:consumer_version_id), Sequel[:cv][:number].as(:consumer_version_number), Sequel[:cv][:order].as(:consumer_version_order),
+      Sequel[:p][:id].as(:provider_id), Sequel[:p][:name].as(:provider_name),
+      Sequel[:pact_publications][:revision_number], Sequel[:pv][:id].as(:pact_version_id), Sequel[:pv][:sha].as(:pact_version_sha), Sequel[:pact_publications][:created_at]).
       join(:versions, {:id => :consumer_version_id}, {:table_alias => :cv, implicit_qualifier: :pact_publications}).
       join(:pacticipants, {:id => :pacticipant_id}, {:table_alias => :c, implicit_qualifier: :cv}).
       join(:pacticipants, {:id => :provider_id}, {:table_alias => :p, implicit_qualifier: :pact_publications}).
