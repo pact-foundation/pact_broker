b39733a196fb95172ac8109a1371852398ef8e85
Beth Skurrie <beth@bethesque.com>
2017-05-19 13:24:05 +1000

    Increment minor version number every time script/publish-new.sh is called

--- script/publish-new.sh
+++ script/publish-new.sh
@@ -1,7 +1,9 @@
 # set -x
 BODY=$(ruby -e "require 'json'; j = JSON.parse(File.read('script/foo-bar.json')); j['interactions'][0]['providerState'] = 'it is ' + Time.now.to_s; puts j.to_json")
+latest_url=$(curl http://localhost:9292/pacts/provider/Bar/consumer/Foo/latest | jq -r ._links.self.href)
+next_version=$(echo ${latest_url} | ruby -e "require 'semver'; puts SemVer.parse(ARGF.read[/\d+\.\d\.\d+/]).tap{ | v| v.minor = v.minor + 1}.format('%M.%m.%p')")
 echo ${BODY} >> tmp.json
 curl -v -XPUT \-H "Content-Type: application/json" \
 -d@tmp.json \
-http://127.0.0.1:9292/pacts/provider/Bar/consumer/Foo/version/1.0.0
+http://127.0.0.1:9292/pacts/provider/Bar/consumer/Foo/version/${next_version}
 rm tmp.json
