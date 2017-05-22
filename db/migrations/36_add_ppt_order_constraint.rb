8ce6d79dadb492ee4c12c9f932b39d36ff71c292
Beth Skurrie <beth@bethesque.com>
2017-05-22 14:10:37 +1000

    Remove redundant migration scripts.

--- db/migrations/36_add_ppt_order_constraint.rb
+++
@@ -1,7 +0,0 @@
-Sequel.migration do
-  change do
-    alter_table(:versions) do
-      add_index [:pacticipant_id, :order], unique: true, unique_constraint_name: 'uq_ver_ppt_ord'
-    end
-  end
-end
