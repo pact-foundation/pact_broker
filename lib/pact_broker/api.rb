61a83009e51f8615220b9a243357932ddb6be36e
Beth Skurrie <beth@bethesque.com>
2017-08-21 20:07:33 +1000

    chore(webhook status): correct pact_webhooks_status resource name

--- lib/pact_broker/api.rb
+++ lib/pact_broker/api.rb
@@ -49,7 +49,7 @@ module PactBroker
 
         # Webhooks
         add ['webhooks', 'provider', :provider_name, 'consumer', :consumer_name ], Api::Resources::PactWebhooks, {resource_name: "pact_webhooks"}
-        add ['webhooks', 'provider', :provider_name, 'consumer', :consumer_name, 'status' ], Api::Resources::PactWebhooksStatus, {resource_name: "pact_webhooks"}
+        add ['webhooks', 'provider', :provider_name, 'consumer', :consumer_name, 'status' ], Api::Resources::PactWebhooksStatus, {resource_name: "pact_webhooks_status"}
 
         add ['webhooks', :uuid ], Api::Resources::Webhook, {resource_name: "webhook"}
         add ['webhooks', :uuid, 'trigger', :trigger_uuid, 'logs' ], Api::Resources::TriggeredWebhookLogs, {resource_name: "triggered_webhook_logs"}
