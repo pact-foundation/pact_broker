920c3635ae90b446e36479e0568db2773c14ba7b
Beth Skurrie <beth@bethesque.com>
2017-09-07 14:20:25 +1000

    fix: sequel migration 25 for mysql

--- db/migrations/25_make_pv_pacticipants_mandatory.rb
+++ db/migrations/25_make_pv_pacticipants_mandatory.rb
@@ -1,8 +1,16 @@
 Sequel.migration do
   up do
+    PactBroker::MigrationHelper.with_mysql do
+      run("SET FOREIGN_KEY_CHECKS = 0")
+    end
+
     alter_table(:pact_versions) do
       set_column_not_null(:consumer_id)
       set_column_not_null(:provider_id)
+    end
+
+    PactBroker::MigrationHelper.with_mysql do
+      run("SET FOREIGN_KEY_CHECKS = 1")
     end
   end
 end
