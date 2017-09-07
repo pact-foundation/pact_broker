0ee48e10099b67df5ad7ef25b1c2229f042f7c74
Beth Skurrie <beth@bethesque.com>
2017-09-07 14:15:23 +1000

    fix: sequel migration 19 for mysql
    Closes: https://github.com/pact-foundation/pact_broker/issues/33

--- db/migrations/19_make_pact_version_content_sha_not_nullable.rb
+++ db/migrations/19_make_pact_version_content_sha_not_nullable.rb
@@ -2,9 +2,17 @@ require 'digest/sha1'
 require_relative 'migration_helper'
 
 Sequel.migration do
-  change do
+  up do
+    PactBroker::MigrationHelper.with_mysql do
+      run("SET FOREIGN_KEY_CHECKS = 0")
+    end
+
     alter_table(:pacts) do
       set_column_not_null(:pact_version_content_sha)
+    end
+
+    PactBroker::MigrationHelper.with_mysql do
+      run("SET FOREIGN_KEY_CHECKS = 1")
     end
   end
 end
