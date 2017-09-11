2f5df826f80c04279c521d4513369035a758d1e1
Beth Skurrie <beth@bethesque.com>
2017-09-11 10:53:50 +1000

    chore: script

--- script/db-spec.sh
+++ script/db-spec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cd db/test/backwards_compatibility
-export BUNDLE_GEMFILE="$(pwd)/Gemfile"
+export BUNDLE_GEMFILE=$PWD/Gemfile
 bundle install
 bundle exec appraisal install
 bundle exec rake
