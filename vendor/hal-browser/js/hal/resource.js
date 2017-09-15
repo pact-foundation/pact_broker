354374cd686a4e59ebc30c2e0449b15c72061158
Beth Skurrie <beth@bethesque.com>
2017-09-15 16:51:15 +1000

    feat(hal browser): use name and title from self link when not specified in embedded resource

--- vendor/hal-browser/js/hal/resource.js
+++ vendor/hal-browser/js/hal/resource.js
@@ -2,14 +2,28 @@ HAL.Models.Resource = Backbone.Model.extend({
   initialize: function(representation) {
     representation = representation || {};
     this.links = representation._links;
-    this.title = representation.title;
-    this.name = representation.name;
+    this.title = this.buildTitle(representation);
+    this.name = this.buildName(representation);
     if(representation._embedded !== undefined) {
       this.embeddedResources = this.buildEmbeddedResources(representation._embedded);
     }
     this.set(representation);
     this.unset('_embedded', { silent: true });
     this.unset('_links', { silent: true });
+  },
+
+  buildName: function(representation) {
+    return representation.name ||
+      (representation._links
+        && representation._links.self
+        && representation._links.self.name);
+  },
+
+  buildTitle: function(representation) {
+    return representation.title ||
+      (representation._links
+        && representation._links.self
+        && representation._links.self.title);
   },
 
   buildEmbeddedResources: function(embeddedResources) {
