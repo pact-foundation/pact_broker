912f5dc5787a31f1a6658296ba80c502be12ff2a
Beth Skurrie <beth@bethesque.com>
2017-10-10 20:26:06 +1100

    style: refactor query to determine compatible versions
    Change query so that we're not likely to hit the limits on the maximum number
    of items in a SQL IN clause when there are many versions for a pacticipant.

--- lib/pact_broker/matrix/repository.rb
+++ lib/pact_broker/matrix/repository.rb
@@ -7,42 +7,42 @@ module PactBroker
       include PactBroker::Repositories
 
       def find pacticipant_1_name, pacticipant_2_name
-        version_ids = PactBroker::Domain::Version.select(Sequel[:versions][:id])
-          .join(:pacticipants, id: :pacticipant_id)
-          .where(Sequel[:pacticipants][:name] => [pacticipant_1_name, pacticipant_2_name])
-        find_for_version_ids(version_ids)
+        find_for_version_ids([], [pacticipant_1_name, pacticipant_2_name])
           .sort{|l1, l2| l2[:consumer_version_order] <=> l1[:consumer_version_order]}
       end
 
       ##
-      # candidate_versions Hash of candidate name to version
+      # candidate_versions Hash of pacticipant name => version
+      # Ihe value is nil, it means all versions for that pacticipant are to be included
       # Returns a list of matrix lines indicating the compatible versions
+      #
       def find_compatible_pacticipant_versions criteria
-        version_ids = criteria.collect do | key, value |
-          if value
-            version_repository.find_by_pacticipant_name_and_number(key, value).id
-          else
-            pacticipant_repository.find_by_name(key).versions.collect(&:id)
-          end
-        end.flatten
+        version_ids = criteria.reject{ |key, value| !value }.collect do | key, value |
+          version_repository.find_by_pacticipant_name_and_number(key, value).id
+        end
 
-        find_for_version_ids(version_ids)
+        pacticipant_names = criteria.reject{|key, value| value }.keys
+
+        find_for_version_ids(version_ids, pacticipant_names)
           .group_by{|line| [line[:consumer_version_number], line[:provider_version_number]]}
           .values
           .collect(&:last)
           .select{ |line | line[:success] }
       end
 
-      def find_for_version_ids version_ids
+      def find_for_version_ids version_ids, pacticipant_names = []
         PactBroker::Pacts::LatestPactPublicationsByConsumerVersion
           .select_append(:consumer_version_number, :provider_name, :consumer_name, :provider_version_id, :provider_version_number, :success)
           .select_append(Sequel[:latest_pact_publications_by_consumer_versions][:created_at].as(:pact_created_at))
           .select_append(Sequel[:all_verifications][:number])
           .select_append(Sequel[:all_verifications][:id].as(:verification_id))
-          .select_append(Sequel[:all_verifications][:execution_date].as(:verification_executed_at))
+          .select_append(Sequel[:execution_date].as(:verification_executed_at))
           .left_outer_join(:all_verifications, pact_version_id: :pact_version_id)
-          .where(provider_version_id: version_ids).or(provider_version_id: nil)
+          .where(provider_version_id: version_ids)
+            .or(provider_version_id: nil)
+            .or(provider_name: pacticipant_names)
           .where(consumer_version_id: version_ids)
+            .or(consumer_name: pacticipant_names)
           .order(:execution_date, :verification_id)
           .collect(&:values)
       end
