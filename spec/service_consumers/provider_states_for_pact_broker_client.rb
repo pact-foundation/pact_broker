075ed8967cf9ad77378e8399e2b54ebc3bb6c2b0
Beth Skurrie <beth@bethesque.com>
2017-11-06 17:00:35 +1100

    test: add new provider state

--- spec/service_consumers/provider_states_for_pact_broker_client.rb
+++ spec/service_consumers/provider_states_for_pact_broker_client.rb
@@ -57,20 +57,30 @@ Pact.provider_states_for "Pact Broker Client" do
   end
 
   provider_state "the pact for Foo version 1.2.3 has been successfully verified by Bar version 4.5.6 with tag prod, and 1.2.4 unsuccessfully by 9.9.9" do
-      set_up do
-        TestDataBuilder.new
-          .create_pact_with_hierarchy("Foo", "1.2.3", "Bar")
-          .revise_pact
-          .create_verification(provider_version: "4.5.6")
-          .use_provider("Bar")
-          .use_provider_version("4.5.6")
-          .create_provider_version_tag("prod")
-          .create_consumer_version("1.2.4")
-          .create_pact
-          .revise_pact
-          .create_verification(provider_version: "9.9.9", success: false)
-      end
+    set_up do
+      TestDataBuilder.new
+        .create_pact_with_hierarchy("Foo", "1.2.3", "Bar")
+        .revise_pact
+        .create_verification(provider_version: "4.5.6")
+        .use_provider("Bar")
+        .use_provider_version("4.5.6")
+        .create_provider_version_tag("prod")
+        .create_consumer_version("1.2.4")
+        .create_pact
+        .revise_pact
+        .create_verification(provider_version: "9.9.9", success: false)
     end
+  end
+
+  provider_state "the pact for Foo version 1.2.3 has been verified by Bar version 4.5.6 and version 5.6.7" do
+    set_up do
+      TestDataBuilder.new
+        .create_pact_with_hierarchy("Foo", "1.2.3", "Bar")
+        .revise_pact
+        .create_verification(provider_version: "4.5.6")
+        .create_verification(provider_version: "5.6.7", number: 2)
+    end
+  end
 
   provider_state "the 'Pricing Service' does not exist in the pact-broker" do
     no_op
