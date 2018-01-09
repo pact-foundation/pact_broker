69f860b7689c0491685c18620635ffeecf27b645
Beth Skurrie <beth@bethesque.com>
2018-01-09 12:01:57 +1100

    chore: add script to insert a self signed certificate from a URL

---
+++ script/insert-self-signed-certificate-from-url.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'uri'
+require 'securerandom'
+require 'logger'
+require 'sequel'
+
+uri_string = ARGV[0]
+database = ARGV[1]
+raise "Usage: #{__FILE__} URI SQLITE_DATABASE_PATH" unless uri_string && database
+
+DATABASE_CREDENTIALS = {logger: Logger.new($stdout), adapter: "sqlite", database: database, :encoding => 'utf8'}
+
+uri = URI(uri_string)
+connection = Sequel.connect(DATABASE_CREDENTIALS)
+
+certificate_command = "openssl s_client -showcerts -servername #{uri.host} -connect #{uri.host}:#{uri.port} </dev/null 2>/dev/null | openssl x509 -outform PEM"
+certificate_content = `#{certificate_command}`
+
+puts "Downloaded certificate from #{uri.host}: #{certificate_content}"
+
+certificate_hash = {
+  uuid: SecureRandom.urlsafe_base64,
+  description: "Self signed certificate for #{uri.host}",
+  content: certificate_content,
+  created_at: DateTime.now,
+  updated_at: DateTime.now
+}
+
+connection[:certificates].insert(certificate_hash)
+
+puts "Done"
