73f5ca2783b8fb173e054e19262cd647d14fa0f6
Beth Skurrie <beth@bethesque.com>
2018-01-15 14:56:25 +1100

    chore: echo server logs on travis when backwards compatibility tests fail
    Trying to debug flakey tests

--- script/db-spec.sh
+++ script/db-spec.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 cd db/test/backwards_compatibility
+rm -rf log
 export BUNDLE_GEMFILE=$PWD/Gemfile
 bundle install
 bundle exec appraisal update
 bundle exec appraisal install
+set +e
 bundle exec rake
+rake_exit_code=$?
+if [[ $rake_exit_code -ne 0 && -n "$TRAVIS" ]]; then
+  cat log/pact_broker.log
+fi
