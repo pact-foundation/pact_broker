01fd2f27ae75efe4f586c6765bd7c9ba2cee14fe
Beth Skurrie <beth@bethesque.com>
2018-01-20 13:43:07 +1100

    refactor: new line

--- db/migrations/000046_recreate_latest_verifications.rb
+++ db/migrations/000046_recreate_latest_verifications.rb
@@ -11,7 +11,8 @@ Sequel.migration do
       "SELECT v.id, v.number, v.success, s.number as provider_version, v.build_url, v.pact_version_id, v.execution_date, v.created_at, v.provider_version_id, s.number as provider_version_number
         FROM verifications v
         INNER JOIN latest_verification_numbers lv
-          ON v.pact_version_id = lv.pact_version_id AND v.number = lv.latest_number
+          ON v.pact_version_id = lv.pact_version_id
+          AND v.number = lv.latest_number
         INNER JOIN versions s on v.provider_version_id = s.id"
     )
   end
