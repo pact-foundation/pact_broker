3f13d2bd59783ec329cbbc60eeac293ac4d6b49b
Beth Skurrie <beth@bethesque.com>
2018-01-23 09:48:12 +1100

    refactor: update hash syntax

--- db/migrations/000004_create_tags_table.rb
+++ db/migrations/000004_create_tags_table.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     create_table(:tags, charset: 'utf8') do
       String :name
       foreign_key :version_id, :versions
-      primary_key [:version_id, :name], :name=>:tags_pk
+      primary_key [:version_id, :name], name: :tags_pk
     end
   end
 end
