317a64de7429bb2fa7101d931a828c322de81e17
Beth Skurrie <beth@bethesque.com>
2018-01-29 21:21:34 +1100

    feat(dashboard api): add plain text content type for easy debugging

--- lib/pact_broker/api/resources/dashboard.rb
+++ lib/pact_broker/api/resources/dashboard.rb
@@ -1,5 +1,6 @@
 require 'pact_broker/api/resources/base_resource'
 require 'pact_broker/api/decorators/dashboard_decorator'
+require 'pact_broker/api/decorators/dashboard_text_decorator'
 
 module PactBroker
   module Api
@@ -8,7 +9,10 @@ module PactBroker
       class Dashboard < BaseResource
 
         def content_types_provided
-          [["application/hal+json", :to_json]]
+          [
+            ["application/hal+json", :to_json],
+            ["text/plain", :to_text],
+          ]
         end
 
         def allowed_methods
@@ -17,6 +21,10 @@ module PactBroker
 
         def to_json
           PactBroker::Api::Decorators::DashboardDecorator.new(index_items).to_json(user_options: decorator_context)
+        end
+
+        def to_text
+          PactBroker::Api::Decorators::DashboardTextDecorator.new(index_items).to_text(user_options: decorator_context)
         end
 
         private
