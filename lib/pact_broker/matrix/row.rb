f138ea9e8fab14483af6179aff2efcb199200884
Beth Skurrie <beth@bethesque.com>
2018-01-29 12:06:56 +1100

    chore: delete unused file

--- lib/pact_broker/matrix/row.rb
+++ lib/pact_broker/matrix/row.rb
@@ -1,6 +1,5 @@
 require 'pact_broker/repositories/helpers'
 require 'pact_broker/webhooks/latest_triggered_webhook'
-require 'pact_broker/tags/latest_verification_tag'
 require 'pact_broker/tags/tag_with_latest_flag'
 
 module PactBroker
