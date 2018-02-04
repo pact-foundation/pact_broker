825af6d6ab8843c191431026998f34c112eac01e
Beth Skurrie <beth@bethesque.com>
2018-02-05 10:37:46 +1100

    feat(matrix ui): correct logic for hiding pact links

--- lib/pact_broker/ui/view_models/matrix_lines.rb
+++ lib/pact_broker/ui/view_models/matrix_lines.rb
@@ -15,7 +15,7 @@ module PactBroker
           # Don't have a URL to view ovewritten pact revisions, so don't show a link for them until we do
           line_group_ids = []
           each do | line |
-            line_group_id = [line.consumer_name, line.consumer_version_number, line.provider_name, line.provider_version_number]
+            line_group_id = [line.consumer_name, line.consumer_version_number, line.provider_name]
             if line_group_ids.include?(line_group_id)
               line.overwritten = true
             else
