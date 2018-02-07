011d7c904db8f77469dc38de03e11eaeab28d540
Beth Skurrie <beth@bethesque.com>
2018-02-08 08:38:44 +1100

    feat(matrix): speed up query to refresh index

--- db/migrations/20180210_fix_latest_matrix_for_cv_and_pv_again.rb
+++ db/migrations/20180210_fix_latest_matrix_for_cv_and_pv_again.rb
@@ -8,10 +8,21 @@ Sequel.migration do
     # lose the unverified pacts.
     # This view used to be (stupidly) called latest_matrix
 
-    # Fixes mistakenly copied definition in 20180209_recreate_latest_matrix_for_cv_and_pv_union_all.rb
-    # which missed the join to latest_pact_publication_revision_numbers
+    # Fix mistakenly copied definition in 20180209_recreate_latest_matrix_for_cv_and_pv_union_all.rb
+    # which missed the join to latest_pact_publication_revision_numbers.
+
+    # Change this view to be based on materialized_matrix instead of matrix
+    # to speed it up.
+    # Note! This does mean there is a dependency on having updated
+    # materialized_matrix FIRST that may cause problems. Will see how it goes.
+
+    alter_table(:materialized_matrix) do
+      add_index [:verification_id], name: 'ndx_mm_verif_id'
+      add_index [:pact_revision_number], name: 'ndx_mm_pact_rev_num'
+    end
+
     create_or_replace_view(:latest_matrix_for_consumer_version_and_provider_version,
-      "SELECT matrix.* FROM matrix
+      "SELECT matrix.* FROM materialized_matrix matrix
       inner join latest_pact_publication_revision_numbers lr
       on matrix.consumer_id = lr.consumer_id
       and matrix.provider_id = lr.provider_id
@@ -24,7 +35,7 @@ Sequel.migration do
 
       UNION ALL
 
-      select matrix.* from matrix
+      select matrix.* from materialized_matrix matrix
       inner join latest_pact_publication_revision_numbers lr
       on matrix.consumer_id = lr.consumer_id
       and matrix.provider_id = lr.provider_id
