d2992cf33d7e15219d1eb98303d9c14b6e123720
Beth Skurrie <beth@bethesque.com>
2018-03-10 20:02:07 +1100

    chore: allow pact publish script to have custom consumer/provider names

--- script/publish.sh
+++ script/publish.sh
@@ -1,4 +1,15 @@
+#!/bin/sh
+function finish {
+  rm -rf tmp/pact.json
+}
+trap finish EXIT
+
+consumer=${1:-Foo}
+provider=${2:-Bar}
+echo $consumer $provider
+body=$(cat script/foo-bar.json | sed "s/Foo/${consumer}/" | sed "s/Bar/${provider}/")
+echo $body > tmp/pact.json
 curl -v -XPUT \-H "Content-Type: application/json" \
--d@script/foo-bar.json \
-http://127.0.0.1:9292/pacts/provider/Bar/consumer/Foo/version/1.0.0
+-d@tmp/pact.json \
+http://127.0.0.1:9292/pacts/provider/${provider}/consumer/${consumer}/version/1.0.0
 echo ""
