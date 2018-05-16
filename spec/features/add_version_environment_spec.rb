dd328265b2ddeb20305faafb4527ea722597d7c2
Beth Skurrie <beth@bethesque.com>
2018-05-16 19:29:45 +1000

    test: add test for invalid environment

--- spec/features/add_version_environment_spec.rb
+++ spec/features/add_version_environment_spec.rb
@@ -7,11 +7,19 @@ describe "adding environment for a pacticipant version" do
 
   subject { put path, nil, {'CONTENT_TYPE' => 'application/json'}; last_response }
 
-  context "when the pacticipant/version/tag do not exist" do
+  context "when the pacticipant/version/environment do not exist" do
     it "creates an environment" do
       expect{ subject }.to change {
         PactBroker::Environments::Environment.where(name: 'production').count
       }.by(1)
+    end
+  end
+
+  context "when the environment name is not in the list of allowed environments" do
+    let(:path) { "/pacticipants/Foo/versions/1.2.3/environments/prod" }
+
+    it "returns an error" do
+      expect(subject.status).to eq 400
     end
   end
 end
