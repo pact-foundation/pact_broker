83af87b65791e3b913e33e17006ac43b71dcea85
Beth Skurrie <beth@bethesque.com>
2018-05-16 20:05:16 +1000

    feat: add indices for version_environments

--- db/migrations/20180401_create_version_environments.rb
+++ db/migrations/20180401_create_version_environments.rb
@@ -4,8 +4,10 @@ Sequel.migration do
       String :name, null: false
       DateTime :created_at, null: false
       DateTime :updated_at, null: false
-      foreign_key :version_id, :versions, null: false, on_delete: :cascade, foreign_key_constraint_name: "fk_verenv_versions"
-      primary_key [:version_id, :name], name: :environments_pk
+      foreign_key :version_id, :versions, null: false, on_delete: :cascade, foreign_key_constraint_name: 'fk_verenv_versions'
+      primary_key [:version_id, :name], name: 'version_environments_pk'
+      index :name, name: 'ndx_ver_env_name'
+      index [:version_id, :name], unique: true, name: 'ndx_verenv_verid_name'
     end
   end
 end
