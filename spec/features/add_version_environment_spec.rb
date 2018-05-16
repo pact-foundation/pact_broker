5723cbe5e7966fe15224bb12f3075b514fa632e5
Beth Skurrie <beth@bethesque.com>
2018-05-16 19:23:33 +1000

    test: update version environment spec

--- spec/features/add_version_environment_spec.rb
+++ spec/features/add_version_environment_spec.rb
@@ -1,13 +1,16 @@
-
 describe "adding environment for a pacticipant version" do
-  let(:path) { "/pacticipants/Foo/versions/1.2.3/environments/feat%2Fbar" }
+  before do
+    PactBroker.configuration.environments = ["production"]
+  end
+
+  let(:path) { "/pacticipants/Foo/versions/1.2.3/environments/production" }
 
   subject { put path, nil, {'CONTENT_TYPE' => 'application/json'}; last_response }
 
   context "when the pacticipant/version/tag do not exist" do
     it "creates an environment" do
       expect{ subject }.to change {
-        PactBroker::Environments::Environment.where(name: 'feat/bar').count
+        PactBroker::Environments::Environment.where(name: 'production').count
       }.by(1)
     end
   end
