abccf7a7da5590292c950552401fd3f499705f38
Beth Skurrie <beth@bethesque.com>
2018-06-09 13:51:33 +1000

    feat: add ${pactbroker.githubVerificationStatus} to webhook templates

--- lib/pact_broker/webhooks/render.rb
+++ lib/pact_broker/webhooks/render.rb
@@ -1,12 +1,15 @@
 module PactBroker
   module Webhooks
     class Render
-      def self.call(body, pact, verification = nil, &escaper)
+      def self.call(template, pact, verification = nil, &escaper)
         base_url = PactBroker.configuration.base_url
         params = {
           '${pactbroker.pactUrl}' => PactBroker::Api::PactBrokerUrls.pact_url(base_url, pact),
           '${pactbroker.consumerVersionNumber}' => pact.consumer_version_number,
-          '${pactbroker.providerVersionNumber}' => verification ? verification.provider_version_number : ""
+          '${pactbroker.providerVersionNumber}' => verification ? verification.provider_version_number : "",
+          '${pactbroker.consumerName}' => pact.consumer_name,
+          '${pactbroker.providerName}' => pact.provider_name,
+          '${pactbroker.githubVerificationStatus}' => github_verification_status(verification)
         }
 
         if escaper
@@ -15,8 +18,16 @@ module PactBroker
           end
         end
 
-        params.inject(body) do | body, (key, value) |
-          body.gsub(key, value)
+        params.inject(template) do | template, (key, value) |
+          template.gsub(key, value)
+        end
+      end
+
+      def self.github_verification_status verification
+        if verification
+          verification.success ? "success" : "failure"
+        else
+          ""
         end
       end
     end
--- spec/lib/pact_broker/webhooks/render_spec.rb
+++ spec/lib/pact_broker/webhooks/render_spec.rb
@@ -9,43 +9,54 @@ module PactBroker
           allow(PactBroker::Api::PactBrokerUrls).to receive(:pact_url).and_return("http://foo")
         end
 
-        let(:body) do
-          "Foo ${pactbroker.pactUrl} ${pactbroker.consumerVersionNumber} ${pactbroker.providerVersionNumber}"
-        end
-
         let(:pact) do
-          instance_double("pact", consumer_version_number: "1.2.3+foo")
+          instance_double("pact", consumer_version_number: "1.2.3+foo", consumer_name: "Foo", provider_name: "Bar")
         end
 
         let(:verification) do
-          instance_double("verification", provider_version_number: "3")
+          instance_double("verification", provider_version_number: "3", success: true)
         end
 
-        subject { Render.call(body, pact, verification) }
+        let(:failed_verification) do
+          instance_double("verification", provider_version_number: "3", success: false)
+        end
 
-        it { is_expected.to eq "Foo http://foo 1.2.3+foo 3" }
+        let(:nil_verification) { nil }
 
+        subject { Render.call(template, pact, verification) }
 
-        context "when the verification is nil" do
-          let(:verification) { nil }
+        TEST_CASES = [
+          ["${pactbroker.pactUrl}", "http://foo", :pact, :verification],
+          ["${pactbroker.consumerVersionNumber}", "1.2.3+foo", :pact, :verification],
+          ["${pactbroker.providerVersionNumber}", "3", :pact, :verification],
+          ["${pactbroker.providerVersionNumber}", "", :pact, :nil_verification],
+          ["${pactbroker.consumerName}", "Foo", :pact, :verification],
+          ["${pactbroker.providerName}", "Bar", :pact, :verification],
+          ["${pactbroker.githubVerificationStatus}", "success", :pact, :verification],
+          ["${pactbroker.githubVerificationStatus}", "failure", :pact, :failed_verification],
+          ["${pactbroker.githubVerificationStatus}", "", :pact, :nil_verification]
+        ]
 
-          let(:body) do
-            "${pactbroker.providerVersionNumber}"
-          end
-
-          it "inserts an empty string" do
-            expect(subject).to eq ""
+        TEST_CASES.each do | (template, expected_output, pact_var_name, verification_var_name) |
+          it "replaces #{template} with #{expected_output.inspect}" do
+            the_pact = send(pact_var_name)
+            the_verification = send(verification_var_name)
+            output = Render.call(template, the_pact, the_verification)
+            expect(output).to eq expected_output
           end
         end
 
         context "with an escaper" do
           subject do
-            Render.call(body, pact, verification) do | value |
+            Render.call(template, pact, verification) do | value |
               CGI.escape(value)
             end
           end
+          let(:template) do
+            "${pactbroker.pactUrl}"
+          end
 
-          it { is_expected.to eq "Foo http%3A%2F%2Ffoo 1.2.3%2Bfoo 3" }
+          it { is_expected.to eq "http%3A%2F%2Ffoo" }
         end
       end
     end
