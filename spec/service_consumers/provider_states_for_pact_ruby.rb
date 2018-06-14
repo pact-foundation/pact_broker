463994e16c1e823311ba959a272e5ad33a5120ce
Rashi Agarwal <ag15.rashi@gmail.com>
2018-06-14 22:17:20 +1000

    converting double quotes to single quotes

--- spec/service_consumers/provider_states_for_pact_ruby.rb
+++ spec/service_consumers/provider_states_for_pact_ruby.rb
@@ -6,24 +6,24 @@ Pact.provider_states_for "Pact Ruby" do
     no_op
   end
 
-  provider_state "consumer-1 and consumer-2 have pacts with provider provider-1" do
+  provider_state 'consumer-1 and consumer-2 have pacts with provider provider-1' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_pact
-        .create_consumer("consumer-2")
+        .create_consumer('consumer-2')
         .create_consumer_version('1.4.0')
         .create_pact
     end
   end
 
-  provider_state "consumer-1 and consumer-2 have pacts with provider provider-1 tagged with tag-1" do
+  provider_state 'consumer-1 and consumer-2 have pacts with provider provider-1 tagged with tag-1' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_consumer_version_tag('tag-1')
         .create_pact
@@ -34,59 +34,59 @@ Pact.provider_states_for "Pact Ruby" do
     end
   end
 
-  provider_state "consumer-1 and consumer-2 have pacts with provider provider-1 tagged with tag-2" do
+  provider_state 'consumer-1 and consumer-2 have pacts with provider provider-1 tagged with tag-2' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_consumer_version_tag('tag-2')
         .create_pact
-        .create_consumer("consumer-2")
+        .create_consumer('consumer-2')
         .create_consumer_version('1.4.0')
         .create_consumer_version_tag('tag-2')
         .create_pact
     end
   end
 
-  provider_state "consumer-1 and consumer-2 have 2 pacts with provider provider-1 tagged with tag-1" do
+  provider_state 'consumer-1 and consumer-2 have 2 pacts with provider provider-1 tagged with tag-1' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_consumer_version_tag('tag-1')
         .create_pact
-        .create_consumer("consumer-2")
+        .create_consumer('consumer-2')
         .create_consumer_version('1.4.0')
         .create_consumer_version_tag('tag-1')
         .create_pact
     end
   end
 
-  provider_state "consumer-1 and consumer-2 have 2 pacts with provider provider-1 tagged with tag-2" do
+  provider_state 'consumer-1 and consumer-2 have 2 pacts with provider provider-1 tagged with tag-2' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_consumer_version_tag('tag-2')
         .create_pact
-        .create_consumer("consumer-2")
+        .create_consumer('consumer-2')
         .create_consumer_version('1.4.0')
         .create_consumer_version_tag('tag-2')
         .create_pact
     end
   end
 
-  provider_state "consumer-1 and consumer-2 have 2 pacts with provider provider-1" do
+  provider_state 'consumer-1 and consumer-2 have 2 pacts with provider provider-1' do
     set_up do
       TestDataBuilder.new
-        .create_provider("provider-1")
-        .create_consumer("consumer-1")
+        .create_provider('provider-1')
+        .create_consumer('consumer-1')
         .create_consumer_version('1.3.0')
         .create_pact
-        .create_consumer("consumer-2")
+        .create_consumer('consumer-2')
         .create_consumer_version('1.4.0')
         .create_pact
     end
