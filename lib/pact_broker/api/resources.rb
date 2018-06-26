14131c5da3abf323ccf410b1b619edac76231243
Beth Skurrie <beth@bethesque.com>
2018-06-27 07:42:59 +1000

    chore: require base_resource.rb before requiring any other resource files

--- lib/pact_broker/api/resources.rb
+++ lib/pact_broker/api/resources.rb
@@ -5,6 +5,8 @@ Reform::Form.class_eval do
   feature Reform::Form::Dry
 end
 
-Dir.glob(File.expand_path(File.join(__FILE__, "..", "resources", "*.rb"))) do |file|
-  require file
+require 'pact_broker/api/resources/base_resource'
+
+Dir.glob(File.expand_path(File.join(__FILE__, "..", "resources", "*.rb"))).sort.each do | path |
+  require path
 end
