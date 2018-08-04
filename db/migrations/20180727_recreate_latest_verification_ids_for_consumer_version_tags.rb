70165b4fd70de10a28acccb3b13cf21b6ddbddbc
Beth Skurrie <beth@bethesque.com>
2018-08-04 15:03:19 +1000

    chore: correct logic for latest_verification_ids_for_consumer_version_tags

--- db/migrations/20180727_recreate_latest_verification_ids_for_consumer_version_tags.rb
+++ db/migrations/20180727_recreate_latest_verification_ids_for_consumer_version_tags.rb
@@ -1,32 +1,17 @@
 Sequel.migration do
   up do
-    ltcvo = :latest_tagged_pact_consumer_version_orders
-    versions_join = {
-      Sequel[ltcvo][:consumer_id] => Sequel[:cv][:pacticipant_id],
-      Sequel[ltcvo][:latest_consumer_version_order] => Sequel[:cv][:order]
-    }
-    lpp_join = {
-      Sequel[:lpp][:consumer_version_id] => Sequel[:cv][:id],
-      Sequel[ltcvo][:provider_id] => Sequel[:lpp][:provider_id]
-    }
-    # todo add pact_version_id to latest_pact_publication_ids_by_consumer_versions?
-    pp_join = {
-      Sequel[:pp][:id] => Sequel[:lpp][:pact_publication_id]
-    }
-    verifications_join = {
-      Sequel[:v][:pact_version_id] => Sequel[:pp][:pact_version_id]
-    }
-    view = from(ltcvo).select_group(
-          Sequel[ltcvo][:provider_id],
-          Sequel[ltcvo][:consumer_id],
-          Sequel[ltcvo][:tag_name].as(:consumer_version_tag_name))
-        .select_append{ max(v[id]).as(latest_verification_id) }
-        .join(:versions, versions_join, { table_alias: :cv } )
-        .join(:latest_pact_publication_ids_by_consumer_versions, lpp_join, { table_alias: :lpp })
-        .join(:pact_publications, pp_join, { table_alias: :pp })
-        .join(:verifications, verifications_join, { table_alias: :v })
-
-    create_or_replace_view(:latest_verification_ids_for_consumer_version_tags, view)
+    create_or_replace_view(:latest_verification_ids_for_consumer_version_tags,
+    "select
+      v.provider_id,
+      v.consumer_id,
+      t.name as consumer_version_tag_name,
+      max(v.verification_id) as latest_verification_id
+    from latest_verif_id_for_pact_version_and_provider_version v
+    join latest_pact_publication_ids_by_consumer_versions lpp
+      on v.pact_version_id = lpp.pact_version_id
+    join tags t
+      on lpp.consumer_version_id = t.version_id
+    group by v.provider_id, v.consumer_id, t.name")
   end
 
   down do
