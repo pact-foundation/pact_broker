c1026b1c44a6e2ffcb933bae27450406bc7877d9
Beth Skurrie <beth@bethesque.com>
2018-08-05 15:57:12 +1000

    chore: add config for postgres running on docker on port 5433

--- script/recreate-docker-pg-db.sh
+++ script/recreate-docker-pg-db.sh
@@ -1,18 +1,20 @@
-# docker run -p "5432:5432" --cpus 0.2 postgres:9.5-alpine
+# docker run -p "5433:5432" --cpus 0.2 postgres:9.5-alpine
 
 SCHEMA="pact_broker"
 set +e
 export PACT_BROKER_DATABASE_HOST="127.0.0.1"
-psql postgres -h $PACT_BROKER_DATABASE_HOST -U postgres -c "DROP DATABASE ${SCHEMA};"
-psql postgres -h $PACT_BROKER_DATABASE_HOST -U postgres -c "CREATE DATABASE ${SCHEMA};"
-psql postgres -h $PACT_BROKER_DATABASE_HOST -U postgres -c "CREATE USER pact_broker WITH PASSWORD 'pact_broker'"
-psql postgres -h $PACT_BROKER_DATABASE_HOST -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${SCHEMA} to pact_broker;"
-psql postgres -h $PACT_BROKER_DATABASE_HOST -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${SCHEMA} TO pact_broker;"
+export PACT_BROKER_DATABASE_PORT="5433"
+psql postgres -h $PACT_BROKER_DATABASE_HOST -p $PACT_BROKER_DATABASE_PORT -U postgres -c "DROP DATABASE ${SCHEMA};"
+psql postgres -h $PACT_BROKER_DATABASE_HOST -p $PACT_BROKER_DATABASE_PORT -U postgres -c "CREATE DATABASE ${SCHEMA};"
+psql postgres -h $PACT_BROKER_DATABASE_HOST -p $PACT_BROKER_DATABASE_PORT -U postgres -c "CREATE USER pact_broker WITH PASSWORD 'pact_broker'"
+psql postgres -h $PACT_BROKER_DATABASE_HOST -p $PACT_BROKER_DATABASE_PORT -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${SCHEMA} to pact_broker;"
+psql postgres -h $PACT_BROKER_DATABASE_HOST -p $PACT_BROKER_DATABASE_PORT -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${SCHEMA} TO pact_broker;"
 echo ""
 echo "run the following command to set your environment variables:"
 echo "export PACT_BROKER_DATABASE_USERNAME=pact_broker"
 echo "export PACT_BROKER_DATABASE_PASSWORD=pact_broker"
 echo "export PACT_BROKER_DATABASE_NAME=${SCHEMA}"
 echo "export PACT_BROKER_DATABASE_HOST=${ip}"
+echo "export PACT_BROKER_DATABASE_PORT=${PACT_BROKER_DATABASE_PORT}"
 echo "To test:"
-echo "psql -h \$PACT_BROKER_DATABASE_HOST -d \$PACT_BROKER_DATABASE_NAME -U \$PACT_BROKER_DATABASE_USERNAME"
+echo "psql -h \$PACT_BROKER_DATABASE_HOST -p \$PACT_BROKER_DATABASE_PORT -d \$PACT_BROKER_DATABASE_NAME -U \$PACT_BROKER_DATABASE_USERNAME"
