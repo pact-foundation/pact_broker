01dd86696a6174f3f350926889e159eca38d240e
Beth Skurrie <beth@bethesque.com>
2018-08-28 07:39:43 +1000

    feat: eager load pacticipant labels for /pacticipants resource

--- lib/pact_broker/pacticipants/repository.rb
+++ lib/pact_broker/pacticipants/repository.rb
@@ -23,7 +23,7 @@ module PactBroker
       def find options = {}
         query = PactBroker::Domain::Pacticipant.select_all_qualified
         query = query.label(options[:label_name]) if options[:label_name]
-        query.order_ignore_case(Sequel[:pacticipants][:name]).all
+        query.order_ignore_case(Sequel[:pacticipants][:name]).eager(:labels).all
       end
 
       def find_all_pacticipant_versions_in_reverse_order name
