07652b50953987e58296a4dc5f95af5434eee99c
Beth Skurrie <beth@bethesque.com>
2019-04-15 08:26:53 +1000

    test: add spec for deleting integration

---
+++ spec/features/delete_integration_spec.rb
@@ -0,0 +1,27 @@
+describe "Deleting pact versions" do
+
+  let(:path) { "/integrations/provider/Bar/consumer/Foo" }
+
+  subject { delete(path)  }
+
+  context "when the integration exists" do
+    before do
+      TestDataBuilder.new
+        .create_pact_with_hierarchy("Foo", "1.2.3", "Bar")
+    end
+
+    it "deletes the resource associated with the integration" do
+      expect{ subject }.to change{ PactBroker::Pacts::PactPublication.count }.by(-1)
+    end
+
+    it "returns a 204" do
+      expect(subject.status).to be 204
+    end
+  end
+
+  context "when the pact does not exist" do
+    it "returns a 404 Not Found" do
+      expect(subject.status).to be 404
+    end
+  end
+end
