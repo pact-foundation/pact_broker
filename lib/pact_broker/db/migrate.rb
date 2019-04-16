692a8ab07d52ea4d5987d3f92899eec66f87a908
Beth Skurrie <beth@bethesque.com>
2019-04-16 20:34:25 +1000

    chore: use logger instead of puts to record running migrations

--- lib/pact_broker/db/migrate.rb
+++ lib/pact_broker/db/migrate.rb
@@ -1,5 +1,6 @@
 require 'sequel'
 require 'pact_broker/project_root'
+require 'pact_broker/logging'
 Sequel.extension :migration
 
 module PactBroker
@@ -7,7 +8,7 @@ module PactBroker
     class Migrate
       def self.call database_connection, options = {}
         db_migrations_dir = PactBroker.project_root.join('db','migrations')
-        puts "Running migrations in directory #{db_migrations_dir}, target=#{options.fetch(:target, 'end')}"
+        PactBroker.logger.info "Running migrations in directory #{db_migrations_dir}, target=#{options.fetch(:target, 'end')}"
         Sequel::TimestampMigrator.new(database_connection, db_migrations_dir, options).run
       end
     end
