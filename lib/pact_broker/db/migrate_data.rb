f829be615822c4150bb46ccfe122cc8f6af77aac
Beth Skurrie <beth@bethesque.com>
2019-04-16 20:13:10 +1000

    docs: document data migrations

--- lib/pact_broker/db/migrate_data.rb
+++ lib/pact_broker/db/migrate_data.rb
@@ -2,6 +2,14 @@ Dir.glob(File.expand_path(File.join(__FILE__, "..", "data_migrations", "*.rb")))
   require path
 end
 
+# Data migrations run *every* time the broker starts up, after the schema migrations.
+# Their purpose is to ensure that data integrity is maintained during rolling migrations
+# in architectures with multiple application instances running against the same
+# database (eg. EC2 autoscaling group) where "old" data might be inserted by
+# the application instance running the previous version of the code AFTER
+# the schema migrations have been run on the first application instance with the
+# new version of the code.
+
 module PactBroker
   module DB
     class MigrateData
