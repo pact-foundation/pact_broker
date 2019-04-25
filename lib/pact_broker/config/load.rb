72997a77268cd88028d46e1e3d395d1625641e66
Beth Skurrie <beth@bethesque.com>
2019-04-26 09:36:57 +1000

    refactor: config setting code

--- lib/pact_broker/config/load.rb
+++ lib/pact_broker/config/load.rb
@@ -34,26 +34,9 @@ module PactBroker
       def set_value_on_configuration setting
         if configuration_attribute_exists? setting
           logger.debug("Loading #{setting.name} configuration from database.")
-          configuration.send("#{setting.name}=", get_value_from_setting(setting))
+          configuration.send("#{setting.name}=", setting.value_object)
         else
           logger.warn("Could not load configuration setting \"#{setting.name}\" as there is no matching attribute on the Configuration class")
-        end
-      end
-
-      def get_value_from_setting setting
-        case setting.type
-        when 'json'
-          JSON.parse(setting.value, symbolize_names: true)
-        when 'string'
-          setting.value
-        when 'integer'
-          Integer(setting.value)
-        when 'float'
-          Float(setting.value)
-        when 'space_delimited_string_list'
-          SpaceDelimitedStringList.parse(setting.value)
-        when 'boolean'
-          setting.value == "1"
         end
       end
     end
--- lib/pact_broker/config/setting.rb
+++ lib/pact_broker/config/setting.rb
@@ -1,6 +1,66 @@
 module PactBroker
   module Config
     class Setting < Sequel::Model(:config)
+
+      def set_value_from(object)
+        self.type = Setting.get_db_type(object)
+        self.value = Setting.get_db_value(object)
+        self
+      end
+
+      def value_object
+        case type
+        when 'json'
+          JSON.parse(value, symbolize_names: true)
+        when 'string'
+          value
+        when 'integer'
+          Integer(value)
+        when 'float'
+          Float(value)
+        when 'space_delimited_string_list'
+          SpaceDelimitedStringList.parse(value)
+        when 'boolean'
+          value == "1"
+        end
+      end
+
+      def self.get_db_value(object)
+        case object
+        when String, Integer, Float, NilClass
+          object
+        when TrueClass
+          "1"
+        when FalseClass
+          "0"
+        when SpaceDelimitedStringList
+          object.to_s
+        when Array, Hash
+          object.to_json
+        else
+          nil
+        end
+      end
+
+      def self.get_db_type(object)
+        case object
+          when true, false
+            'boolean'
+          when String, nil
+            'string'
+          when SpaceDelimitedStringList
+            'space_delimited_string_list'
+          when Array, Hash
+            'json'
+          when Integer
+            'integer'
+          when Float
+            'float'
+          else
+            nil
+          end
+      end
+
     end
 
     Setting.plugin :timestamps, update_on_create: true
