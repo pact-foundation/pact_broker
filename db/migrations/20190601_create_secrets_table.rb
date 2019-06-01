d6827510ee68445b29debf49f976991c3924a497
Beth Skurrie <beth@bethesque.com>
2019-06-02 08:07:41 +1000

    feat(secrets): add description to secret

--- db/migrations/20190601_create_secrets_table.rb
+++ db/migrations/20190601_create_secrets_table.rb
@@ -4,6 +4,7 @@ Sequel.migration do
       primary_key :id
       String :uuid, null: false, unique: true, unique_constraint_name: 'uq_secrets_uuid'
       String :name, null: false, unique: true, unique_constraint_name: 'uq_secrets_name'
+      String :description
       String :encrypted_value, null: false
       String :encrypted_value_iv, null: false, unique: true, unique_constraint_name: 'uq_secrets_value_iv'
       String :key_id
