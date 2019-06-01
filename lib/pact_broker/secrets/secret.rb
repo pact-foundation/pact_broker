8271294a674ba2a08b5b18cff91bd9c9d3d27005
Beth Skurrie <beth@bethesque.com>
2019-06-02 09:42:20 +1000

    chore: update Sequel annotations

--- lib/pact_broker/secrets/secret.rb
+++ lib/pact_broker/secrets/secret.rb
@@ -22,3 +22,21 @@ module PactBroker
     end
   end
 end
+
+# Table: secrets
+# Columns:
+#  id                 | integer                     | PRIMARY KEY DEFAULT nextval('secrets_id_seq'::regclass)
+#  uuid               | text                        | NOT NULL
+#  name               | text                        | NOT NULL
+#  description        | text                        |
+#  encrypted_value    | text                        | NOT NULL
+#  encrypted_value_iv | text                        | NOT NULL
+#  key_id             | text                        |
+#  algorithm          | text                        | NOT NULL
+#  created_at         | timestamp without time zone | NOT NULL
+#  updated_at         | timestamp without time zone | NOT NULL
+# Indexes:
+#  secrets_pkey        | PRIMARY KEY btree (id)
+#  uq_secrets_name     | UNIQUE btree (name)
+#  uq_secrets_uuid     | UNIQUE btree (uuid)
+#  uq_secrets_value_iv | UNIQUE btree (encrypted_value_iv)
