7d3b9e92aabc940b957cb770c4f30fb050da77a5
Beth Skurrie <beth@bethesque.com>
2019-06-02 10:56:53 +1000

    refactor: extract webhook request and response logging into separate class

--- lib/pact_broker/domain/webhook_request.rb
+++ lib/pact_broker/domain/webhook_request.rb
@@ -52,93 +52,17 @@ module PactBroker
       end
     end
 
-    class WebhookRequest
+    class WebhookRequestLogger
+      attr_reader :execution_logger, :logger, :uuid, :options
 
-      include PactBroker::Logging
-      include PactBroker::Messages
-      HEADERS_TO_REDACT = [/authorization/i, /token/i]
-
-      attr_accessor :method, :url, :headers, :body, :username, :password, :uuid
-
-      # Reform gets confused by the :method method, as :method is a standard
-      # Ruby method.
-      alias_method :http_method, :method
-
-      def initialize attributes = {}
-        @method = attributes[:method]
-        @url = attributes[:url]
-        @username = attributes[:username]
-        @password = attributes[:password]
-        @headers = attributes[:headers] || {}
-        @body = attributes[:body]
-        @uuid = attributes[:uuid]
-      end
-
-      def description
-        "#{method.upcase} #{URI(url).host}"
-      end
-
-      def display_password
-        password.nil? ? nil : "**********"
-      end
-
-      def redacted_headers
-        headers.each_with_object({}) do | (name, value), new_headers |
-          redact = HEADERS_TO_REDACT.any?{ | pattern | name =~ pattern }
-          new_headers[name] = redact ? "**********" : value
-        end
-      end
-
-      def execute options = {}
+      def initialize(logger, execution_logger, uuid, options)
+        @logger = logger
+        @execution_logger = execution_logger
+        @uuid = uuid
         @options = options
-        @logs = StringIO.new
-        @execution_logger = Logger.new(logs)
-        begin
-          execute_and_build_result
-        rescue StandardError => e
-          handle_error_and_build_result(e)
-        end
       end
 
-      private
-
-      attr_reader :options, :execution_logger, :logs
-
-      def execute_and_build_result
-        log_request
-        response = do_request
-        log_response(response)
-        result = WebhookExecutionResult.new(WebhookRequestWithRedactedHeaders.new(http_request), response, logs.string)
-        log_completion_message(result.success?)
-        result
-      end
-
-      def handle_error_and_build_result e
-        log_error(e)
-        log_completion_message(false)
-        WebhookExecutionResult.new(WebhookRequestWithRedactedHeaders.new(http_request), nil, logs.string, e)
-      end
-
-      def http_request
-        @http_request ||= begin
-          req = Net::HTTP.const_get(method.capitalize).new(url)
-          headers.each_pair { | name, value | req[name] = value }
-          req.basic_auth(username, password) if username && username.size > 0
-          req.body = body unless body.nil?
-          req
-        end
-      end
-
-      def do_request
-        options = PactBroker::BuildHttpOptions.call(uri)
-        req = http_request
-        response = Net::HTTP.start(uri.hostname, uri.port, :ENV, options) do |http|
-          http.request req
-        end
-        WebhookResponseWithUtf8SafeBody.new(response)
-      end
-
-      def log_request
+      def log_request(uri, url, http_method, http_request, body)
         redacted_request = WebhookRequestWithRedactedHeaders.new(http_request)
         logger.info "Making webhook #{uuid} request #{http_method.upcase} URI=#{uri} (headers and body in debug logs)"
         logger.debug "Webhook #{uuid} request headers=#{redacted_request.to_hash}"
@@ -204,6 +128,95 @@ module PactBroker
         else
           execution_logger.error "Error executing webhook #{uuid}. #{response_body_hidden_message}"
         end
+      end
+
+    end
+
+    class WebhookRequest
+
+      include PactBroker::Logging
+      include PactBroker::Messages
+      HEADERS_TO_REDACT = [/authorization/i, /token/i]
+
+      attr_accessor :method, :url, :headers, :body, :username, :password, :uuid
+
+      # Reform gets confused by the :method method, as :method is a standard
+      # Ruby method.
+      alias_method :http_method, :method
+
+      def initialize attributes = {}
+        @method = attributes[:method]
+        @url = attributes[:url]
+        @username = attributes[:username]
+        @password = attributes[:password]
+        @headers = attributes[:headers] || {}
+        @body = attributes[:body]
+        @uuid = attributes[:uuid]
+      end
+
+      def description
+        "#{method.upcase} #{URI(url).host}"
+      end
+
+      def display_password
+        password.nil? ? nil : "**********"
+      end
+
+      def redacted_headers
+        headers.each_with_object({}) do | (name, value), new_headers |
+          redact = HEADERS_TO_REDACT.any?{ | pattern | name =~ pattern }
+          new_headers[name] = redact ? "**********" : value
+        end
+      end
+
+      def execute options = {}
+        @options = options
+        @logs = StringIO.new
+        @execution_logger = Logger.new(logs)
+        @webhook_request_logger = WebhookRequestLogger.new(logger, execution_logger, uuid, options)
+        begin
+          execute_and_build_result
+        rescue StandardError => e
+          handle_error_and_build_result(e)
+        end
+      end
+
+      private
+
+      attr_reader :options, :execution_logger, :logs, :webhook_request_logger
+
+      def execute_and_build_result
+        webhook_request_logger.log_request(uri, url, http_method, http_request, body)
+        response = do_request
+        webhook_request_logger.log_response(response)
+        result = WebhookExecutionResult.new(WebhookRequestWithRedactedHeaders.new(http_request), response, logs.string)
+        webhook_request_logger.log_completion_message(result.success?)
+        result
+      end
+
+      def handle_error_and_build_result e
+        webhook_request_logger.log_error(e)
+        webhook_request_logger.log_completion_message(false)
+        WebhookExecutionResult.new(WebhookRequestWithRedactedHeaders.new(http_request), nil, logs.string, e)
+      end
+
+      def http_request
+        @http_request ||= begin
+          req = Net::HTTP.const_get(method.capitalize).new(url)
+          headers.each_pair { | name, value | req[name] = value }
+          req.basic_auth(username, password) if username && username.size > 0
+          req.body = body unless body.nil?
+          req
+        end
+      end
+
+      def do_request
+        options = PactBroker::BuildHttpOptions.call(uri)
+        req = http_request
+        response = Net::HTTP.start(uri.hostname, uri.port, :ENV, options) do |http|
+          http.request req
+        end
+        WebhookResponseWithUtf8SafeBody.new(response)
       end
 
       def to_s
