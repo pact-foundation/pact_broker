a03fbf8009ccdd87b4f1321b23272f72e79b9631
Beth Skurrie <beth@bethesque.com>
2019-06-03 08:15:58 +1000

    style: whitespace

--- db/migrations/20190510_set_version_sequence.rb
+++ db/migrations/20190510_set_version_sequence.rb
@@ -1,4 +1,5 @@
 require 'pact_broker/db/data_migrations/set_latest_version_sequence_value'
+
 Sequel.migration do
   up do
     PactBroker::DB::DataMigrations::SetLatestVersionSequenceValue.call(self)
