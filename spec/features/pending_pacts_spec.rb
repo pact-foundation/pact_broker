c0e46f8eb475a985ae96d85b468784af1dc03901
Beth Skurrie <beth@bethesque.com>
2019-06-04 17:02:18 +1000

    refactor: pending pacts spec

--- spec/features/pending_pacts_spec.rb
+++ spec/features/pending_pacts_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "the pending pacts flow" do
+RSpec.describe "the pending lifecycle of a pact (with no tags)" do
   let(:pact_content_1) { { some: "interactions" }.to_json }
   let(:pact_content_2) { { some: "other interactions" }.to_json }
   let(:request_headers) { { "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT" => "application/hal+json"} }
@@ -15,46 +15,98 @@ RSpec.describe "the pending pacts flow" do
     }.to_json
   end
 
-  context "with no tags" do
-    it "is pending until verified" do
-      # CONSUMER BUILD
-      # publish pact
-      publish_response = put("/pacts/provider/Bar/consumer/Foo/version/1", pact_content_1, request_headers)
+  def publish_pact
+    put("/pacts/provider/Bar/consumer/Foo/version/1", pact_content_1, request_headers)
+  end
 
-      # PROVIDER BUILD 1
-      # get pacts for verification
-      for_verification_response = get("/pacts/provider/Bar/for-verification", nil, request_headers)
-      pending = JSON.parse(for_verification_response.body)["_embedded"]["pacts"][0]["verificationProperties"]["pending"]
-      expect(pending).to be true
+  def get_pacts_for_verification
+    get("/pacts/provider/Bar/for-verification", nil, request_headers)
+  end
 
-      # fetch pact
-      pact_url = JSON.parse(last_response.body)["_embedded"]["pacts"][0]["_links"]["self"]["href"]
-      pact_response = get pact_url, nil, request_headers
+  def pact_url_from(pacts_for_verification_response)
+    JSON.parse(pacts_for_verification_response.body)["_embedded"]["pacts"][0]["_links"]["self"]["href"]
+  end
 
-      # verify pact... failure...
+  def get_pact(pact_url)
+    get pact_url, nil, request_headers
+  end
 
-      # publish failure verification results
-      verification_results_url = JSON.parse(pact_response.body)["_links"]["pb:publish-verification-results"]["href"]
-      post(verification_results_url, failed_verification_results, request_headers)
+  def verification_results_url_from(pact_response)
+    JSON.parse(pact_response.body)["_links"]["pb:publish-verification-results"]["href"]
+  end
 
-      # PROVIDER BUILD 2
-      # get pacts for verification
-      for_verification_response = get("/pacts/provider/Bar/for-verification", nil, request_headers)
-      pending = JSON.parse(for_verification_response.body)["_embedded"]["pacts"][0]["verificationProperties"]["pending"]
-      # still pending
-      expect(pending).to be true
+  def publish_verification_results(verification_results_url, results)
+    post(verification_results_url, results, request_headers)
+  end
 
-      # verify pact... success!
+  def pending_from(pacts_for_verification_response)
+    JSON.parse(pacts_for_verification_response.body)["_embedded"]["pacts"][0]["verificationProperties"]["pending"]
+  end
 
-      # publish successful verification results
-      post(verification_results_url, successful_verification_results, request_headers)
 
-      # PROVIDER BUILD 3
-      # get pacts for verification
-      for_verification_response = get("/pacts/provider/Bar/for-verification", nil, request_headers)
-      pending = JSON.parse(for_verification_response.body)["_embedded"]["pacts"][0]["verificationProperties"]["pending"]
-      # not pending any more
-      expect(pending).to be false
+  context "a pact" do
+    describe "when it is first published" do
+      it "is pending" do
+        publish_pact
+        pacts_for_verification_response = get_pacts_for_verification
+        pending = pending_from(pacts_for_verification_response)
+        expect(pending).to be true
+      end
+    end
+
+    describe "when it is verified unsuccessfully" do
+      it "is still pending" do
+        # CONSUMER BUILD
+        # publish pact
+        publish_pact
+
+        # PROVIDER BUILD
+        # fetch pacts to verify
+        pacts_for_verification_response = get_pacts_for_verification
+        pact_url = pact_url_from(pacts_for_verification_response)
+        pact_response = get_pact(pact_url)
+
+        # verify pact... failure...
+
+        # publish failure verification results
+        verification_results_url = verification_results_url_from(pact_response)
+        publish_verification_results(verification_results_url, failed_verification_results)
+
+        # ANOTHER PROVIDER BUILD
+        # get pacts for verification
+        pacts_for_verification_response = get_pacts_for_verification
+        pending = pending_from(pacts_for_verification_response)
+        # still pending
+        expect(pending).to be true
+      end
+    end
+
+    describe "when it is verified successfully" do
+      it "is no longer pending" do
+        # CONSUMER BUILD
+        publish_pact
+
+        # PROVIDER BUILD
+        pacts_for_verification_response = get_pacts_for_verification
+
+        # fetch pact
+        pact_url = pact_url_from(pacts_for_verification_response)
+        pact_response = get_pact(pact_url)
+
+        # verify pact... success!
+
+        # publish failure verification results
+        verification_results_url = verification_results_url_from(pact_response)
+        publish_verification_results(verification_results_url, successful_verification_results)
+
+        # ANOTHER PROVIDER BUILD 2
+        # get pacts for verification
+        # publish successful verification results
+        pacts_for_verification_response = get_pacts_for_verification
+        pending = pending_from(pacts_for_verification_response)
+        # not pending any more
+        expect(pending).to be false
+      end
     end
   end
 end
