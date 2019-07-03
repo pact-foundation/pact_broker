116f8eaad89562a0bdf9c9e820967300201f7a33
Beth Skurrie <beth@bethesque.com>
2019-07-03 20:03:07 +1000

    feat: ensure clean up script handles pact versions for left over verifications

--- script/prod/clean-up.sql
+++ script/prod/clean-up.sql
@@ -2,10 +2,10 @@
 -- and cleans up orphan pacticipant versions and their tags.
 -- Also removes webhook execution history.
 
-DELETE FROM verifications WHERE created_at < now() - '60 days'::interval;
 DELETE FROM webhook_executions;
 DELETE FROM triggered_webhooks;
+DELETE FROM verifications WHERE created_at < now() - '60 days'::interval;
 DELETE FROM pact_publications WHERE created_at < now() - '60 days'::interval;
-DELETE FROM pact_versions WHERE id NOT IN (SELECT DISTINCT pact_version_id FROM pact_publications);
+DELETE FROM pact_versions WHERE id NOT IN (SELECT pact_version_id FROM pact_publications UNION SELECT pact_version_id FROM verifications);
 DELETE FROM tags WHERE version_id NOT IN (SELECT consumer_version_id FROM pact_publications UNION SELECT provider_version_id FROM verifications);
 DELETE FROM versions WHERE id NOT IN (SELECT consumer_version_id FROM pact_publications UNION SELECT provider_version_id FROM verifications);
