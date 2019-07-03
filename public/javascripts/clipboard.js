0caf123d20d2b28b4a6823815a1d732c78323315
Tan Le <tanle.oz@gmail.com>
2019-07-03 22:53:51 +1000

    Move clipboard functionalities to a separate file.

---
+++ public/javascripts/clipboard.js
@@ -0,0 +1,36 @@
+// https://gist.github.com/Chalarangelo/4ff1e8c0ec03d9294628efbae49216db#file-copytoclipboard-js
+function copyToClipboard(text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+
+  const selected =
+        document.getSelection().rangeCount > 0
+        ? document.getSelection().getRangeAt(0)
+        : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+}
+
+function clipper() {
+  const elements = $(".clippable");
+
+  elements.hover(function() {
+    $(this).children(".clippy").toggleClass("hidden");
+  });
+
+  elements
+    .children(".clippy")
+    .click(function() {
+      const text = $.trim($(this).closest(".clippable").text());
+      copyToClipboard(text);
+    });
+}
