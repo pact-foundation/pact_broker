045f3f38dee8930167269d66a142f6b6d9d0925d
Beth Skurrie <beth@bethesque.com>
2019-09-12 10:40:56 +1000

    fix: don't drop column from view in down step, because you can't remove a column from a view in posgres

--- db/migrations/20190910_add_test_results_to_latest_verifications.rb
+++ db/migrations/20190910_add_test_results_to_latest_verifications.rb
@@ -32,30 +32,6 @@ Sequel.migration do
   end
 
   down do
-    v = :verifications
-    create_or_replace_view(:latest_verifications,
-      from(v)
-        .select(
-          Sequel[v][:id],
-          Sequel[v][:number],
-          Sequel[v][:success],
-          Sequel[:s][:number].as(:provider_version),
-          Sequel[v][:build_url],
-          Sequel[v][:pact_version_id],
-          Sequel[v][:execution_date],
-          Sequel[v][:created_at],
-          Sequel[v][:provider_version_id],
-          Sequel[:s][:number].as(:provider_version_number),
-          Sequel[:s][:order].as(:provider_version_order))
-        .join(:latest_verification_numbers,
-          {
-            Sequel[v][:pact_version_id] => Sequel[:lv][:pact_version_id],
-            Sequel[v][:number] => Sequel[:lv][:latest_number]
-          }, { table_alias: :lv })
-        .join(:versions,
-          {
-            Sequel[v][:provider_version_id] => Sequel[:s][:id]
-          }, { table_alias: :s })
-    )
+    # do nothing - you can't drop columns from a postgres view
   end
 end
