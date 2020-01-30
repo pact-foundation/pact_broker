ca35f0d18d676dd178a992e4c60b0e85a344c338
Beth Skurrie <beth@bethesque.com>
2020-01-30 16:04:04 +1100

    chore: revert back to local releases

--- script/release.sh
+++ script/release.sh
@@ -5,6 +5,7 @@ bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact_broker/version.rb
 VERSION=$(ruby -r ./lib/pact_broker/version.rb -e "puts PactBroker::VERSION")
 git commit -m "chore(release): version ${VERSION}"
-git tag -a v${VERSION} -m "chore(release): version ${VERSION}"
-git push origin v${VERSION}
-git push origin master
+bundle exec rake release
+# git tag -a v${VERSION} -m "chore(release): version ${VERSION}"
+# git push origin v${VERSION}
+# git push origin master
