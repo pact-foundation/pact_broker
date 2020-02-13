adf2aa605bbd72c3b69de1571fe9b9b8a229ca15
Beth Skurrie <beth@bethesque.com>
2020-02-13 11:06:29 +1100

    refactor: dry up webhook creation spec

--- spec/features/create_webhook_spec.rb
+++ spec/features/create_webhook_spec.rb
@@ -1,13 +1,10 @@
-require 'support/test_data_builder'
-
 describe "Creating a webhook" do
-
   before do
-    TestDataBuilder.new.create_pact_with_hierarchy("Some Consumer", "1", "Some Provider")
+    td.create_pact_with_hierarchy("Some Consumer", "1", "Some Provider")
   end
 
   let(:headers) { {'CONTENT_TYPE' => 'application/json'} }
-  let(:response_body) { JSON.parse(last_response.body, symbolize_names: true)}
+  let(:response_body) { JSON.parse(subject.body, symbolize_names: true)}
   let(:webhook_json) { webhook_hash.to_json }
   let(:webhook_hash) do
     {
@@ -37,40 +34,29 @@ describe "Creating a webhook" do
     context "with invalid attributes" do
       let(:webhook_hash) { {} }
 
-      it "returns a 400" do
-        subject
-        expect(last_response.status).to be 400
+      its(:status) { is_expected.to be 400 }
+
+      it "returns a JSON content type" do
+        expect(subject.headers['Content-Type']).to eq 'application/hal+json;charset=utf-8'
       end
 
-    it "returns a JSON content type" do
-      subject
-      expect(last_response.headers['Content-Type']).to eq 'application/hal+json;charset=utf-8'
-    end
-
       it "returns the validation errors" do
-        subject
         expect(response_body[:errors]).to_not be_empty
       end
     end
 
     context "with valid attributes" do
-      it "returns a 201 response" do
-        subject
-        expect(last_response.status).to be 201
-      end
+      its(:status) { is_expected.to be 201 }
 
       it "returns the Location header" do
-        subject
-        expect(last_response.headers['Location']).to match(%r{http://example.org/webhooks/.+})
+        expect(subject.headers['Location']).to match(%r{http://example.org/webhooks/.+})
       end
 
       it "returns a JSON Content Type" do
-        subject
-        expect(last_response.headers['Content-Type']).to eq 'application/hal+json;charset=utf-8'
+        expect(subject.headers['Content-Type']).to eq 'application/hal+json;charset=utf-8'
       end
 
       it "returns the newly created webhook" do
-        subject
         expect(response_body).to include webhook_hash
       end
     end
@@ -83,10 +69,7 @@ describe "Creating a webhook" do
       webhook_hash[:provider] = { name: "Some Provider" }
     end
 
-    it "returns a 201 response" do
-      subject
-      expect(last_response.status).to be 201
-    end
+    its(:status) { is_expected.to be 201 }
 
     it "creates a webhook without a consumer" do
       subject
@@ -101,10 +84,7 @@ describe "Creating a webhook" do
       webhook_hash[:consumer] = { name: "Some Consumer" }
     end
 
-    it "returns a 201 response" do
-      subject
-      expect(last_response.status).to be 201
-    end
+    its(:status) { is_expected.to be 201 }
 
     it "creates a webhook without a provider" do
       subject
@@ -116,10 +96,7 @@ describe "Creating a webhook" do
   context "with no consumer or provider" do
     let(:path) { "/webhooks" }
 
-    it "returns a 201 response" do
-      subject
-      expect(last_response.status).to be 201
-    end
+    its(:status) { is_expected.to be 201 }
 
     it "creates a webhook without a provider" do
       subject
