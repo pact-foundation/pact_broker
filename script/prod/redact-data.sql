74ed3987f702e9625ab99fbc97de1440de897a85
Beth Skurrie <beth@bethesque.com>
2020-03-04 15:54:57 +1100

    chore: redact tag names in redact-data.sql
    [ci-skip]

--- script/prod/redact-data.sql
+++ script/prod/redact-data.sql
@@ -1,7 +1,15 @@
-update pacticipants set name = 'pacticipant-' || id;
-update pact_versions set content = '{}';
-update verifications set test_results = null;
-delete from webhook_executions;
-delete from triggered_webhooks;
-delete from webhooks;
-delete from certificates;
+UPDATE pacticipants SET name = 'pacticipant-' || id;
+UPDATE pact_versions SET content = '{}';
+UPDATE verifications SET test_results = null;
+DELETE FROM webhook_executions;
+DELETE FROM triggered_webhooks;
+DELETE FROM webhooks;
+DELETE FROM certificates;
+
+UPDATE tags
+SET name = temprow.redacted_name
+FROM
+  ( SELECT name, 'tag-' || ROW_NUMBER () OVER (ORDER BY name) as redacted_name FROM (
+    SELECT DISTINCT name FROM tags WHERE lower(name) NOT IN ('master', 'test', 'dev', 'prod', 'production') ORDER BY name
+  ) as x) as temprow
+WHERE tags.name = temprow.name
