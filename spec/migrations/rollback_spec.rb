4206f8689ada9dd661aba46560af2b86e52f8102
Beth Skurrie <beth@bethesque.com>
2020-03-31 08:16:14 +1100

    test: add a rollback spec

---
+++ spec/migrations/rollback_spec.rb
@@ -0,0 +1,8 @@
+require 'pactflow/users/constants'
+
+describe "migrate and rollback", migration: true do
+  it "doesn't blow up" do
+    PactBroker::Database.migrate
+    PactBroker::Database.migrate(45) # uses an irreversible migration
+  end
+end
