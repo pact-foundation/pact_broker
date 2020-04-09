4e6f428a43c4a7112efea59c49a9d41a88f22a65
Beth Skurrie <beth@bethesque.com>
2020-04-09 18:01:08 +1000

    chore: use safe migration to rollback to

--- spec/migrations/rollback_spec.rb
+++ spec/migrations/rollback_spec.rb
@@ -1,6 +1,6 @@
 describe "migrate and rollback", migration: true do
   it "doesn't blow up" do
     PactBroker::Database.migrate
-    PactBroker::Database.migrate(45) # uses an irreversible migration
+    PactBroker::Database.migrate(20190509) # previous migration uses an irreversible migration
   end
 end
