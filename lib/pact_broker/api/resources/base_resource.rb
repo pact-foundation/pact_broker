41ee82879dc41e10b92b44fb140495fb42591988
Beth Skurrie <beth@bethesque.com>
2020-07-14 19:14:31 +1000

    chore: remove class definition check

--- lib/pact_broker/api/resources/base_resource.rb
+++ lib/pact_broker/api/resources/base_resource.rb
@@ -1,9 +1,9 @@
+require 'pact_broker/configuration'
+
 module PactBroker
   module Api
     module Resources
-      if !defined?(BaseResource)
-        BaseResource =  PactBroker.configuration.base_resource_class_factory.call
-      end
+      BaseResource = PactBroker.configuration.base_resource_class_factory.call
     end
   end
 end
