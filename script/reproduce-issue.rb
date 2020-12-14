5208b4e27893a9ed5ee35bf6e5f4571c865d76da
Beth Skurrie <beth@bethesque.com>
2020-12-15 09:41:30 +1100

    chore: update issue reproduction

--- script/reproduce-issue.rb
+++ script/reproduce-issue.rb
@@ -9,31 +9,11 @@ begin
 
   td = PactBroker::Test::HttpTestDataBuilder.new(base_url, { })
   td.delete_integration(consumer: "MyConsumer", provider: "MyProvider")
-    .can_i_deploy(pacticipant: "MyProvider", version: "1", to: "prod")
-    .can_i_deploy(pacticipant: "MyConsumer", version: "1", to: "prod")
-    .publish_pact(consumer: "MyConsumer", consumer_version: "1", provider: "MyProvider", content_id: "111", tag: "feature/a")
-    .can_i_deploy(pacticipant: "MyProvider", version: "1", to: "prod")
+    .publish_pact(consumer: "MyConsumer", consumer_version: "1", provider: "MyProvider", content_id: "111", tag: "main")
     .get_pacts_for_verification(
-      enable_pending: true,
       provider_version_tag: "main",
-      include_wip_pacts_since: "2020-01-01",
       consumer_version_selectors: [{ tag: "main", latest: true }])
-    .verify_pact(
-      index: 0,
-      provider_version_tag: "main",
-      provider_version: "1",
-      success: false
-    )
-    .get_pacts_for_verification(
-      enable_pending: true,
-      provider_version_tag: "main",
-      include_wip_pacts_since: "2020-01-01",
-      consumer_version_selectors: [{ tag: "main", latest: true }])
-    .can_i_deploy(pacticipant: "MyProvider", version: "1", to: "prod")
-    .can_i_deploy(pacticipant: "MyConsumer", version: "1", to: "prod")
-    .deploy_to_prod(pacticipant: "MyProvider", version: "1")
-    .can_i_deploy(pacticipant: "MyConsumer", version: "1", to: "prod")
-    .deploy_to_prod(pacticipant: "MyConsumer", version: "1")
+
 
 rescue StandardError => e
   puts "#{e.class} #{e.message}"
