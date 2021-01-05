bb38feba7df9f725076277c1b0122c4323679fc8
Beth Skurrie <beth@bethesque.com>
2021-01-05 15:59:57 +1100

    chore: update decorator configuration

--- lib/pact_broker/api/decorators/configuration.rb
+++ lib/pact_broker/api/decorators/configuration.rb
@@ -7,16 +7,26 @@ module PactBroker
         using PactBroker::StringRefinements
 
         def initialize(overrides = {})
-          @overrides = {}
+          @overrides = overrides
         end
 
         def class_for(name)
-          @overrides[name] || Object.const_get("PactBroker::Api::Decorators::#{name.to_s.camelcase(true)}")
+          if overrides[name].is_a?(String)
+            Object.const_get(overrides[name])
+          elsif overrides[name].is_a?(Class)
+            overrides[name]
+          else
+           Object.const_get("PactBroker::Api::Decorators::#{name.to_s.camelcase(true)}")
+          end
         end
 
         def self.default_configuration
           Configuration.new
         end
+
+        private
+
+        attr_reader :overrides
       end
     end
   end
