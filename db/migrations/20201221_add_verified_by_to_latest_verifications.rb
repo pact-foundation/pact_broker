f353e0fc91d488543ea916fc269ec852099e9721
Ronald Holshausen <rholshausen@dius.com.au>
2021-01-10 11:56:26 +1100

    chore: add down migrations for updated views

--- db/migrations/20201221_add_verified_by_to_latest_verifications.rb
+++ db/migrations/20201221_add_verified_by_to_latest_verifications.rb
@@ -34,6 +34,33 @@ Sequel.migration do
   end
 
   down do
-    # do nothing - you can't drop columns from a postgres view
+    v = :verifications
+    create_or_replace_view(:latest_verifications,
+     from(v)
+       .select(
+         Sequel[v][:id],
+         Sequel[v][:number],
+         Sequel[v][:success],
+         Sequel[:s][:number].as(:provider_version),
+         Sequel[v][:build_url],
+         Sequel[v][:pact_version_id],
+         Sequel[v][:execution_date],
+         Sequel[v][:created_at],
+         Sequel[v][:provider_version_id],
+         Sequel[:s][:number].as(:provider_version_number),
+         Sequel[:s][:order].as(:provider_version_order),
+         Sequel[v][:test_results],
+         Sequel.lit('').as(:verified_by_implementation),
+         Sequel.lit('').as(:verified_by_version))
+       .join(:latest_verification_numbers,
+             {
+               Sequel[v][:pact_version_id] => Sequel[:lv][:pact_version_id],
+               Sequel[v][:number] => Sequel[:lv][:latest_number]
+             }, { table_alias: :lv })
+       .join(:versions,
+             {
+               Sequel[v][:provider_version_id] => Sequel[:s][:id]
+             }, { table_alias: :s })
+    )
   end
 end
--- db/migrations/20201221_add_verified_by_to_latest_verifications_for_pact_versions.rb
+++ db/migrations/20201221_add_verified_by_to_latest_verifications_for_pact_versions.rb
@@ -31,6 +31,32 @@ Sequel.migration do
   end
 
   down do
-    # do nothing - you can't drop columns from a postgres view
+    v = :verifications
+    create_or_replace_view(:latest_verifications_for_pact_versions,
+     from(v)
+       .select(
+         Sequel[v][:id],
+         Sequel[v][:number],
+         Sequel[v][:success],
+         Sequel[v][:build_url],
+         Sequel[v][:pact_version_id],
+         Sequel[v][:execution_date],
+         Sequel[v][:created_at],
+         Sequel[v][:provider_version_id],
+         Sequel[:s][:number].as(:provider_version_number),
+         Sequel[:s][:order].as(:provider_version_order),
+         Sequel[v][:test_results],
+         Sequel.lit('').as(:verified_by_implementation),
+         Sequel.lit('').as(:verified_by_version))
+       .join(:latest_verification_ids_for_pact_versions,
+             {
+               Sequel[v][:pact_version_id] => Sequel[:lv][:pact_version_id],
+               Sequel[v][:id] => Sequel[:lv][:latest_verification_id]
+             }, { table_alias: :lv })
+       .join(:versions,
+             {
+               Sequel[v][:provider_version_id] => Sequel[:s][:id]
+             }, { table_alias: :s })
+    )
   end
 end
