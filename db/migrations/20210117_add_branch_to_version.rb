f28035c181fb4bec990127624bcc70052952ea42
Beth Skurrie <beth@bethesque.com>
2021-01-17 12:59:24 +1100

    chore: add versions_pacticipant_id_branch_order_index

--- db/migrations/20210117_add_branch_to_version.rb
+++ db/migrations/20210117_add_branch_to_version.rb
@@ -1,6 +1,9 @@
 Sequel.migration do
   change do
-    add_column(:versions, :branch, String)
-    add_column(:versions, :build_url, String)
+    alter_table(:versions) do
+      add_column(:branch, String)
+      add_column(:build_url, String)
+      add_index([:pacticipant_id, :branch, :order], name: "versions_pacticipant_id_branch_order_index")
+    end
   end
 end
