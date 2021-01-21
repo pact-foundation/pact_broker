0f948de5fe65e18cabc40586c39c379bbe6566e4
Beth Skurrie <beth@bethesque.com>
2021-01-21 13:10:41 +1100

    feat(verification results): redirect requests for verification results to the HAL browser if requested in a web browser

--- lib/pact_broker/api/paths.rb
+++ lib/pact_broker/api/paths.rb
@@ -4,8 +4,13 @@ module PactBroker
       PACT_BADGE_PATH =         %r{^/pacts/provider/[^/]+/consumer/.*/badge(?:\.[A-Za-z]+)?$}.freeze
       MATRIX_BADGE_PATH =       %r{^/matrix/provider/[^/]+/latest/[^/]+/consumer/[^/]+/latest/[^/]+/badge(?:\.[A-Za-z]+)?$}.freeze
       CAN_I_DEPLOY_BADGE_PATH = %r{^/pacticipants/[^/]+/latest-version/[^/]+/can-i-deploy/to/[^/]+/badge(?:\.[A-Za-z]+)?$}.freeze
+      VERIFICATION_RESULTS =    %r{^/pacts/provider/[^/]+/consumer/[^/]+/pact-version/[^/]+/verification-results/[^/]+}
 
       extend self
+
+      def is_verification_results_path?(path)
+        path.start_with?('/pacts') && (path =~ VERIFICATION_RESULTS)
+      end
 
       def is_badge_path?(path)
         # Optimise by checking include? first - regexp slow
