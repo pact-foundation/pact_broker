10211f275f2939bf11a2ef24b0aa6294c8f327fd
Beth Skurrie <beth@bethesque.com>
2021-02-08 14:12:17 +1100

    chore: add custom index for tag ordering

--- db/migrations/20210205_add_pacticipant_id_to_tag.rb
+++ db/migrations/20210205_add_pacticipant_id_to_tag.rb
@@ -5,10 +5,13 @@ include PactBroker::MigrationHelper
 Sequel.migration do
   change do
     alter_table(:tags) do
+      # TODO set_column_not_null(:pacticipant_id)
+      # TODO set_column_not_null(:version_order)
       add_column(:pacticipant_id, Integer)
       add_column(:version_order, Integer)
-      add_index(:version_id, name: "ndx_tags_version_id")
-      add_index(:pacticipant_id, name: "ndx_tags_pacticipant_id")
+      add_index(:version_order, name: "tags_version_order_index")
+      add_index(:version_id, name: "tags_version_id_index")
+      add_index(:pacticipant_id, name: "tags_pacticipant_id_index")
     end
   end
 end
---
+++ db/migrations/20210206_add_index_to_tags.rb
@@ -0,0 +1,21 @@
+require_relative 'migration_helper'
+
+include PactBroker::MigrationHelper
+
+Sequel.migration do
+  up do
+    if postgres?
+      run("CREATE INDEX tags_pacticipant_id_name_version_order_index ON tags (pacticipant_id, name, version_order DESC);")
+    else
+      alter_table(:tags) do
+        add_index([:pacticipant_id, :name, :version_order], name: "tags_pacticipant_id_name_version_order_index")
+      end
+    end
+  end
+
+  down do
+    alter_table(:tags) do
+      drop_index([:pacticipant_id, :name, :version_order], name: "tags_pacticipant_id_name_version_order_index")
+    end
+  end
+end
