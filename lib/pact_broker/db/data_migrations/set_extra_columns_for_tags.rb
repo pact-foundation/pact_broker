2adeb84ac77160d62883aac1579943d81765e932
Beth Skurrie <beth@bethesque.com>
2021-02-19 13:52:23 +1100

    chore: only set extra columns for tags where not already set

--- lib/pact_broker/db/data_migrations/set_extra_columns_for_tags.rb
+++ lib/pact_broker/db/data_migrations/set_extra_columns_for_tags.rb
@@ -9,7 +9,7 @@ module PactBroker
         def self.call(connection)
           if columns_exist?(connection, :tags, [:version_id, :pacticipant_id]) &&
               columns_exist?(connection, :versions, [:id, :pacticipant_id])
-            connection[:tags].update(
+            connection[:tags].where(pacticipant_id: nil).update(
               pacticipant_id: connection[:versions].select(:pacticipant_id)
                                 .where(Sequel[:versions][:id] => Sequel[:tags][:version_id])
             )
@@ -17,7 +17,7 @@ module PactBroker
 
           if columns_exist?(connection, :tags, [:version_id, :version_order]) &&
               columns_exist?(connection, :versions, [:id, :order])
-            connection[:tags].update(
+            connection[:tags].where(version_order: nil).update(
               version_order: connection[:versions].select(:order)
                                 .where(Sequel[:versions][:id] => Sequel[:tags][:version_id])
             )
