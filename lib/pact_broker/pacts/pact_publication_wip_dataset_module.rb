1596a3c0ecaeee4178479340ba34a4689cc37f84
Beth Skurrie <beth@bethesque.com>
2021-05-19 17:37:41 +1000

    refactor: wip dataset module

--- lib/pact_broker/pacts/pact_publication_wip_dataset_module.rb
+++ lib/pact_broker/pacts/pact_publication_wip_dataset_module.rb
@@ -11,39 +11,39 @@ module PactBroker
       end
 
       def successfully_verified_by_provider_another_branch_before_this_branch_first_created(provider_id, provider_version_branch)
-        query = from_self(alias: :pp)
+        first_version_for_branch = PactBroker::Domain::Version.first_for_pacticipant_id_and_branch(provider_id, provider_version_branch)
+
+        from_self(alias: :pp)
           .select(Sequel[:pp].*)
           .join_successful_non_wip_verifications_for_provider_id(provider_id)
           .join_provider_versions_for_provider_id(provider_id) do
             Sequel.lit('provider_versions.branch != ?', provider_version_branch)
           end
           .where(Sequel[:pp][:provider_id] => provider_id)
-
-        first_version_for_branch = PactBroker::Domain::Version.first_for_pacticipant_id_and_branch(provider_id, provider_version_branch)
-        query = query.verified_before_date(first_version_for_branch.created_at) if first_version_for_branch
-        query.distinct
+          .verified_before_creation_date_of(first_version_for_branch)
+          .distinct
       end
 
       def successfully_verified_by_provider_tag_when_not_wip(provider_id, provider_tag)
-        from_self(alias: :pp).select(Sequel[:pp].*)
+        from_self(alias: :pp)
+          .select(Sequel[:pp].*)
+          .where(Sequel[:pp][:provider_id] => provider_id)
           .join_successful_non_wip_verifications_for_provider_id(provider_id)
           .join_provider_version_tags_for_tag(provider_tag)
-          .where(Sequel[:pp][:provider_id] => provider_id)
           .distinct
       end
 
       def successfully_verified_by_provider_another_tag_before_this_tag_first_created(provider_id, provider_tag)
-        query = from_self(alias: :pp)
+        first_tag_with_name = PactBroker::Domain::Tag.where(pacticipant_id: provider_id, name: provider_tag).order(:created_at).first
+        from_self(alias: :pp)
           .select(Sequel[:pp].*)
           .where(Sequel[:pp][:provider_id] => provider_id)
           .join_successful_non_wip_verifications_for_provider_id(provider_id)
           .join_provider_version_tags do
             Sequel.lit('provider_tags.name != ?', provider_tag)
           end
-
-        first_tag_with_name = PactBroker::Domain::Tag.where(pacticipant_id: provider_id, name: provider_tag).order(:created_at).first
-        query = query.verified_before_date(first_tag_with_name.created_at) if first_tag_with_name
-        query.distinct
+          .verified_before_creation_date_of(first_tag_with_name)
+          .distinct
       end
 
       protected
@@ -92,6 +92,14 @@ module PactBroker
           Sequel[:provider_versions][:pacticipant_id] => provider_id
         }
         join(:versions, versions_join, { table_alias: :provider_versions }, &block)
+      end
+
+      def verified_before_creation_date_of(record)
+        if record
+          verified_before_date(record.created_at)
+        else
+          self
+        end
       end
     end
   end
