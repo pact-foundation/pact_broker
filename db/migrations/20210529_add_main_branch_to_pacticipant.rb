7fb1e14fe602cdf68cfb52de06956d21c0b2e8ad
Beth Skurrie <beth@bethesque.com>
2021-05-29 16:46:18 +1000

    chore: update migration

--- db/migrations/20210529_add_main_branch_to_pacticipant.rb
+++ db/migrations/20210529_add_main_branch_to_pacticipant.rb
@@ -2,14 +2,24 @@ Sequel.migration do
   up do
     alter_table(:pacticipants) do
       add_column(:main_branch, String)
-      drop_column(:main_development_branches)
     end
+
+    # TODO
+    # alter_table(:pacticipants) do
+    #   drop_column(:main_development_branches)
+    # end
   end
 
   down do
     alter_table(:pacticipants) do
       drop_column(:main_branch)
-      add_column(:main_development_branches, String)
+      # TODO
+      # add_column(:main_development_branches, String)
     end
+
+    # TODO
+    # alter_table(:pacticipants) do
+    #   add_column(:main_development_branches, String)
+    # end
   end
 end
