cea1d981faa218fec3ccaa5edd82266870b47836
Beth Skurrie <beth@bethesque.com>
2021-06-17 14:49:45 +1000

    chore: add script to run webhook server with random errors

--- script/dev/webhook-server-with-random-errors.rb
+++ script/dev/webhook-server-with-random-errors.rb
@@ -1,10 +1,18 @@
 require 'rack/utils'
-STDOUT.sync = true
-puts "Starting webhook server"
+require 'rack'
 
-run -> (env) {
+STDOUT.sync = true
+puts "Starting webhook server which will return random errors"
+
+app = -> (env) {
   status = [200, 500, 500, 500].sample
   puts Rack::Utils.parse_nested_query(env['QUERY_STRING']) if env['QUERY_STRING'] && env['QUERY_STRING'] != ''
   puts env['rack.input'].read
   [status, {"Content-Type" => "text/plain"}, ["Webhook response.\n"]].tap { |it| puts it }
 }
+
+Rack::Server.start(
+ :app => app,
+ :server => 'webrick',
+ :Port => 9393
+)
