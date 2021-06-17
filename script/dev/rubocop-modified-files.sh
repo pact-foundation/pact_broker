0623464ba878e5785bf9ec5508f1c272982d6f5e
Beth Skurrie <beth@bethesque.com>
2021-06-17 14:57:53 +1000

    chore: add .ru to list of files to rubocop

--- script/dev/rubocop-modified-files.sh
+++ script/dev/rubocop-modified-files.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git ls-files -m | xargs ls -1 2>/dev/null | grep -e '\.rb$' -e '\.rake$' | xargs rubocop --except Style/StringLiterals
+git ls-files -m | xargs ls -1 2>/dev/null | grep -e '\.rb$' -e '\.ru$' -e '\.rake$' | xargs rubocop --except Style/StringLiterals
