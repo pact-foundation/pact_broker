2e621e36e5f28fa9b903f61a518e3469c13fb3c4
Beth Skurrie <beth@bethesque.com>
2021-09-02 16:28:45 +1000

    chore: remove domain object usage from migration specs

--- spec/migrations/44_add_provider_version_to_verification_spec.rb
+++ spec/migrations/44_add_provider_version_to_verification_spec.rb
@@ -31,29 +31,26 @@ describe "add provider version relationship to verification (migrate 42-44)", mi
   subject { PactBroker::Database.migrate(46) }
 
   it "creates a version object" do
-    expect { subject }.to change { PactBroker::Domain::Version.count }.by(1)
+    expect { subject }.to change { database[:versions].count }.by(1)
   end
 
   it "sets the foreign key to the new version" do
     subject
-    expect(PactBroker::Domain::Verification.first.provider_version.number).to eq "1.2.3"
-  end
-
-  it "sets the created_at date of the new version to the created_at of the verification" do
-    subject
-    expect(PactBroker::Domain::Verification.first.provider_version.created_at.to_datetime).to eq now
+    provider_version_id = database[:verifications].order(:id).first[:provider_version_id]
+    expect(database[:versions].where(id: provider_version_id).single_record[:number]).to eq "1.2.3"
   end
 
   context "when the version already exists" do
     let!(:provider_version) { create(:versions, {number: "1.2.3", order: 1, pacticipant_id: provider[:id], created_at: now, updated_at: now}) }
 
     it "does not create a version object" do
-      expect { subject }.to_not change { PactBroker::Domain::Version.count }
+      expect { subject }.to_not change { database[:versions].count }
     end
 
     it "sets the foreign key to the existing version" do
       subject
-      expect(PactBroker::Domain::Verification.first.provider_version.number).to eq "1.2.3"
+      provider_version_id = database[:verifications].order(:id).first[:provider_version_id]
+      expect(database[:versions].where(id: provider_version_id).single_record[:number]).to eq "1.2.3"
     end
   end
 end
