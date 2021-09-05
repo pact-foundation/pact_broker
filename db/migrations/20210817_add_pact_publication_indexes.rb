e1b21b1c381fa4e56c1b82f4862131ab043a0566
Beth Skurrie <beth@bethesque.com>
2021-09-05 20:44:03 +1000

    chore: remove unnecessary index

--- db/migrations/20210817_add_pact_publication_indexes.rb
+++
@@ -1,7 +0,0 @@
-Sequel.migration do
-  change do
-    alter_table(:pact_publications) do
-      add_index [:consumer_id, :consumer_version_order], name: :pact_publications_consumer_id_consumer_version_order
-    end
-  end
-end
