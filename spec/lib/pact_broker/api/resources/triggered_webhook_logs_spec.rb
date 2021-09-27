8e96ab550f3de647d3e1a105b328d71fe9fe2e81
Beth Skurrie <beth@bethesque.com>
2021-09-27 15:34:52 +1000

    test: update triggered webhook logs spec

--- spec/lib/pact_broker/api/resources/triggered_webhook_logs_spec.rb
+++ spec/lib/pact_broker/api/resources/triggered_webhook_logs_spec.rb
@@ -15,15 +15,16 @@ module PactBroker
             .create_webhook_execution(logs: "waffle")
         end
 
-        let(:path) { "/webhooks/5432/trigger/1234/logs" }
-
         subject { get(path) }
 
         let(:triggered_webhook_uuid) { PactBroker::Webhooks::TriggeredWebhook.first.uuid }
-        let(:path) { "/triggered-webhooks/#{triggered_webhook_uuid}/logs" }
 
-        it "returns the concatenated webhook execution logs" do
-          expect(subject.body).to eq "foo\nbar"
+        describe "by triggered webhook uuid" do
+          let(:path) { "/triggered-webhooks/1234/logs" }
+
+          it "returns the concatenated webhook execution logs" do
+            expect(subject.body).to eq "foo\nbar"
+          end
         end
       end
     end
