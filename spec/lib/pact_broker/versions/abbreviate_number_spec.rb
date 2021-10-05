c2e32c5e05d4eff90ee589e25fda2ace612789d1
ertrzyiks <mateusz.derks@gmail.com>
2021-10-05 10:00:36 +0200

    fix(matrix): prevent long version names overlfow the table

--- spec/lib/pact_broker/versions/abbreviate_number_spec.rb
+++ spec/lib/pact_broker/versions/abbreviate_number_spec.rb
@@ -8,7 +8,8 @@ module PactBroker
           ["202326572516dea6998a7f311fcaa161c0768fc2", "2023265"],
           ["1.2.3+areallyreallyreallylongexplanation", "1.2.3+areallyreallyreallylongexplanation"],
           ["2516dea6998a7f", "2516dea6998a7f"],
-          ["1.2.3+202326572516dea6998a7f311fcaa161c0768fc2", "1.2.3+2023265"]
+          ["1.2.3+202326572516dea6998a7f311fcaa161c0768fc2", "1.2.3+2023265"],
+          ["this-is-very-long-text-this-is-very-long-text-this-is-very-long-text-this-is-very-long-text", "this-is-very-long-text-this-is-very-lon…-long-text"]
         ]
 
         TEST_CASES.each do |(input, output)|
