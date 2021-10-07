3c2795560e7685da70e60425f9bcc2159526c9e8
Mateusz Derks <mateusz.derks@gmail.com>
2021-10-07 02:27:44 +0200

    fix(matrix): prevent long version names overlfow the table (#511)

--- lib/pact_broker/versions/abbreviate_number.rb
+++ lib/pact_broker/versions/abbreviate_number.rb
@@ -3,10 +3,14 @@ module PactBroker
     class AbbreviateNumber
 
       def self.call version_number
-        if version_number
-          version_number.gsub(/[A-Za-z0-9]{40}/) do | val |
-            val[0..6]
-          end
+        return version_number unless version_number
+
+        # hard limit of max 50 characters
+        version_length = version_number.length
+        return version_number[0...39] + "…" + version_number[version_length - 10...version_length] if version_length > 50
+
+        version_number.gsub(/[A-Za-z0-9]{40}/) do | val |
+          val[0..6]
         end
       end
     end
