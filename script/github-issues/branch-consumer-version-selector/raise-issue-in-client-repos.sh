d6bb444fd6eb347313c8cda4ca689e51cc00206d
Beth Skurrie <beth@bethesque.com>
2021-10-15 13:07:05 +1100

    chore: update github issue scripts

--- script/github-issues/branch-consumer-version-selector/raise-issue-in-client-repos.sh
+++ script/github-issues/branch-consumer-version-selector/raise-issue-in-client-repos.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-repos="pact-js pact-jvm pact-net pact-python scala-pact pact-php pact-go pact-reference pact4s pact-consumer-swift"
+repos="pact-js pact-jvm pact-net pact-python pact-php pact-go pact-reference pact4s scala-pact" #pact_elixir
 
 issue_text_file=$(realpath $(dirname "$0")/issue-text.txt)
 
 for repo in $repos; do
   cd "../${repo}"
-  hub issue create --file "${issue_text_file}" --labels "help wanted,good first issue,enhancement"
+  hub issue create --file "${issue_text_file}" --labels "help wanted,enhancement"
 done
