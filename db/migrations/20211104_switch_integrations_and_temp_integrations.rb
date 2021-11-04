6477eff22d33c7af177529b5db40ad99076bcccf
Beth Skurrie <beth@bethesque.com>
2021-11-04 11:15:49 +1100

    chore: add transaction to down step

--- db/migrations/20211104_switch_integrations_and_temp_integrations.rb
+++ db/migrations/20211104_switch_integrations_and_temp_integrations.rb
@@ -7,17 +7,19 @@ Sequel.migration do
   end
 
   down do
-    rename_table :integrations, :temp_integrations
-    create_view(:integrations,
-      from(:pact_publications)
-        .select(
-          :consumer_id,
-          Sequel[:c][:name].as(:consumer_name),
-          :provider_id,
-          Sequel[:p][:name].as(:provider_name)
-        ).distinct
-        .join(:pacticipants, {:id => :consumer_id}, {:table_alias => :c, implicit_qualifier: :pact_publications})
-        .join(:pacticipants, {:id => :provider_id}, {:table_alias => :p, implicit_qualifier: :pact_publications})
-    )
+    transaction do
+      rename_table :integrations, :temp_integrations
+      create_view(:integrations,
+        from(:pact_publications)
+          .select(
+            :consumer_id,
+            Sequel[:c][:name].as(:consumer_name),
+            :provider_id,
+            Sequel[:p][:name].as(:provider_name)
+          ).distinct
+          .join(:pacticipants, {:id => :consumer_id}, {:table_alias => :c, implicit_qualifier: :pact_publications})
+          .join(:pacticipants, {:id => :provider_id}, {:table_alias => :p, implicit_qualifier: :pact_publications})
+      )
+    end
   end
 end
