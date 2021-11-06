0daa445947dfca99f56b8a29544cc9cfc6b4fe93
Beth Skurrie <beth@bethesque.com>
2021-11-06 14:03:59 +1100

    fix: fix error that occurs in some versions of Sqlite when running 20210702_drop_unused_columns_from_deployed_versions.rb
    Fixes: https://github.com/pact-foundation/pact_broker/issues/521

--- db/migrations/20211101_recreate_all_verifications.rb
+++ db/migrations/20211101_recreate_all_verifications.rb
@@ -1,23 +1,8 @@
+require_relative "../ddl_statements/all_verifications"
+
 Sequel.migration do
   up do
-    # need to fully qualify build_url because versions now has a build_url too.
-    # We don't use this view any more but we get an error when dropping the integrations
-    # view if we don't update this, so it must force some re-calculation to be done.
-    create_or_replace_view(:all_verifications,
-      from(:verifications).select(
-        Sequel[:verifications][:id],
-        Sequel[:verifications][:number],
-        :success,
-        :provider_version_id,
-        Sequel[:v][:number].as(:provider_version_number),
-        Sequel[:v][:order].as(:provider_version_order),
-        Sequel[:verifications][:build_url],
-        :pact_version_id,
-        :execution_date,
-        Sequel[:verifications][:created_at],
-        :test_results
-        ).join(:versions, {id: :provider_version_id}, {:table_alias => :v})
-    )
+    create_or_replace_view(:all_verifications, all_verifications_v2(self))
   end
 
   down do
