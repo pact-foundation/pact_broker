d2e14891826dfa85d17fc5906d26304dd7e7f4dc
Beth Skurrie <beth@bethesque.com>
2022-02-10 11:06:11 +1100

    test: update pact publish path

--- spec/features/publish_pact_spec.rb
+++ spec/features/publish_pact_spec.rb
@@ -1,7 +1,7 @@
 describe "Publishing a pact" do
 
   let(:pact_content) { load_fixture("a_consumer-a_provider.json") }
-  let(:path) { "/pacts/provider/A%20Provider/consumer/A%20Consumer/versions/1.2.3" }
+  let(:path) { "/pacts/provider/A%20Provider/consumer/A%20Consumer/version/1.2.3" }
   let(:response_body_json) { JSON.parse(subject.body) }
   let(:rack_env) do
     {
