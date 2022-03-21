9c0fd3c47a16d52fe2acbb3d7106ce886dbccedd
Beth Skurrie <beth@bethesque.com>
2022-03-21 17:47:20 +1100

    feat: print final value of configuration attribute rather than source value

--- lib/pact_broker/config/runtime_configuration_logging_methods.rb
+++ lib/pact_broker/config/runtime_configuration_logging_methods.rb
@@ -1,8 +1,11 @@
 require "uri"
+require "pact_broker/hash_refinements"
 
 module PactBroker
   module Config
     module RuntimeConfigurationLoggingMethods
+      using PactBroker::HashRefinements
+
       module ClassMethods
         def sensitive_values(*values)
           @sensitive_values ||= []
@@ -19,8 +22,11 @@ module PactBroker
       end
 
       module InstanceMethods
+        # base_url raises a not implemented error
         def log_configuration(logger)
-          to_source_trace.sort_by { |key, _| key }.each { |key, value| log_config_inner(key, value, logger) }
+          to_source_trace.without("base_url").each_with_object({})do | (key, details), new_hash |
+            new_hash[key] = details.merge(value: self.send(key.to_sym))
+          end.sort_by { |key, _| key }.each { |key, value| log_config_inner(key, value, logger) }
         end
 
         def log_config_inner(key, value, logger)
@@ -29,7 +35,7 @@ module PactBroker
           elsif self.class.sensitive_value?(key)
             logger.info "#{key}=#{redact(key, value[:value])} source=#{value[:source]}"
           else
-            logger.info "#{key}=#{value[:value]} source=#{value[:source]}"
+            logger.info "#{key}=#{value[:value].inspect} source=#{value[:source]}"
           end
         end
         private :log_config_inner
