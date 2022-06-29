4084ad14a6f10996c623f3b0854ef18e95d32b32
Beth Skurrie <beth.skurrie@smartbear.com>
2022-06-29 12:57:10 +1000

    chore: remove unused file

--- tasks/database/table_dependency_calculator.rb
+++
@@ -1,8 +0,0 @@
-require "pact_broker/db/table_dependency_calculator"
-
-module PactBroker
-  module Database
-    # For backwards compatibility
-    TableDependencyCalculator = PactBroker::DB::TableDependencyCalculator
-  end
-end
