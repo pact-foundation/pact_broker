8ed8224d2b57abf77e844bc58248834cd7a04055
Beth Skurrie <beth.skurrie@smartbear.com>
2022-12-10 15:34:18 +1100

    style: rubocop

--- lib/pact_broker/api/contracts/utf_8_validation.rb
+++ lib/pact_broker/api/contracts/utf_8_validation.rb
@@ -3,7 +3,7 @@ module PactBroker
     module Contracts
       module UTF8Validation
         def fragment_before_invalid_utf_8_char(string)
-          string.force_encoding('UTF-8').each_char.with_index do | char, index |
+          string.force_encoding("UTF-8").each_char.with_index do | char, index |
             if !char.valid_encoding?
               fragment = index < 100 ? string[0...index] : string[index-100...index]
               return index + 1, fragment
