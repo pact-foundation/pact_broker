0e1c43ddbb17098ff0ceb14b0e84798bcb76446c
Beth Skurrie <beth.skurrie@smartbear.com>
2022-12-15 08:22:58 +1100

    feat: add index to provider_version_id column in latest_verification_id_for_pact_version_and_provider_version

---
+++ db/migrations/20221215_add_prov_ver_id_ndx_to_latest_verifi_id_for_pact_ver_and_prov_ver.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  up do
+    if !mysql?
+      alter_table(:latest_verification_id_for_pact_version_and_provider_version) do
+        add_index([:provider_version_id], name: "latest_verif_id_for_pact_ver_and_prov_ver_prov_ver_id_ndx")
+      end
+    end
+  end
+
+  down do
+    if !mysql?
+      alter_table(:latest_verification_id_for_pact_version_and_provider_version) do
+        drop_index([:provider_version_id], name: "latest_verif_id_for_pact_ver_and_prov_ver_prov_ver_id_ndx")
+      end
+    end
+  end
+end
