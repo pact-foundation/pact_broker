2ce13b7b6fcc862e670dc4e884343ffdf6a7fa7e
Beth Skurrie <beth@bethesque.com>
2023-02-04 19:56:52 +1100

    chore: only register lock once

--- lib/sequel/postgres_advisory_lock.rb
+++ lib/sequel/postgres_advisory_lock.rb
@@ -5,17 +5,18 @@ module Sequel
       @name = name
       @type = type
       @lock_obtained = false
+      if postgres?
+        @database_connection.extension :pg_advisory_lock
+        @database_connection.register_advisory_lock(@name, @type)
+      end
     end
 
     def with_lock
       if postgres?
-        @database_connection.extension :pg_advisory_lock
-        @database_connection.register_advisory_lock(@name, @type)
-        results = @database_connection.with_advisory_lock(@name) do
+        @database_connection.with_advisory_lock(@name) do
           @lock_obtained = true
           yield
         end
-        results
       else
         @lock_obtained = true
         yield
