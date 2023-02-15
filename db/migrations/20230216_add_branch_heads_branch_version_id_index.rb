b1b4cefb568ed67d5a629f951070398ce8e22cb6
Beth Skurrie <beth.skurrie@smartbear.com>
2023-02-16 10:10:26 +1100

    feat: add branch_heads_branch_version_id_index

---
+++ db/migrations/20230216_add_branch_heads_branch_version_id_index.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  up do
+    if !mysql?
+      alter_table(:branch_heads) do
+        add_index([:branch_version_id], name: "branch_heads_branch_version_id_index")
+      end
+    end
+  end
+
+  down do
+    if !mysql?
+      alter_table(:branch_heads) do
+        drop_index([:branch_version_id], name: "branch_heads_branch_version_id_index")
+      end
+    end
+  end
+end
