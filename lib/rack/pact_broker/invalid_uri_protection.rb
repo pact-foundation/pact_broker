b520696938bdef63e559c5c22952b0d1f132092a
Beth Skurrie <beth.skurrie@smartbear.com>
2023-03-07 09:12:33 +1100

    chore: remove use of deprecated URI.decode

--- lib/rack/pact_broker/invalid_uri_protection.rb
+++ lib/rack/pact_broker/invalid_uri_protection.rb
@@ -45,7 +45,7 @@ module Rack
       end
 
       def validate(uri)
-        decoded_path = URI.decode(uri.path)
+        decoded_path = URI.decode_www_form_component(uri.path)
         if decoded_path.include?("\n")
           message("errors.new_line_in_url_path")
         elsif decoded_path.include?("\t")
