d3f060c51776211bbd0a980572c4934f4cca3ce3
Beth Skurrie <beth.skurrie@smartbear.com>
2023-03-09 13:03:41 +1100

    chore: clean up webhook pacticipant contract

--- lib/pact_broker/api/contracts/webhook_pacticipant_contract.rb
+++ lib/pact_broker/api/contracts/webhook_pacticipant_contract.rb
@@ -4,7 +4,6 @@ module PactBroker
   module Api
     module Contracts
       class WebhookPacticipantContract < ::Dry::Validation::Contract
-        Helper = PactBroker::Api::Contracts::ValidationHelpers
         include PactBroker::Api::Contracts::DryValidationMethods
 
         json do
@@ -13,13 +12,13 @@ module PactBroker
         end
 
         register_macro(:name_or_label_required) do
-          if !Helper.provided?(values[:name]) && !Helper.provided?(values[:label])
+          if !provided?(values[:name]) && !provided?(values[:label])
             key(path.keys).failure(validation_message("blank"))
           end
         end
 
         register_macro(:name_and_label_exclusive) do
-          if Helper.provided?(values[:name]) && Helper.provided?(values[:label])
+          if provided?(values[:name]) && provided?(values[:label])
             key([:label]).failure(validation_message("cannot_be_provided_at_same_time", name_1: "name", name_2: "label"))
           end
         end
@@ -28,7 +27,7 @@ module PactBroker
         rule(:name, :label).validate(:name_and_label_exclusive)
 
         rule(:name) do
-          validate_pacticipant_with_name_exists(value, key) if Helper.provided?(value)
+          validate_pacticipant_with_name_exists(value, key) if provided?(value)
         end
       end
     end
