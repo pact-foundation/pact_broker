5225cf7427849cde8f30cfd7346ff86384fce5a6
Beth Skurrie <beth.skurrie@smartbear.com>
2023-03-31 13:24:28 +1100

    chore: fix up arguments in pacticipant decorator

--- spec/lib/pact_broker/api/decorators/pacticipant_decorator_spec.rb
+++ spec/lib/pact_broker/api/decorators/pacticipant_decorator_spec.rb
@@ -63,6 +63,16 @@ module PactBroker
 
           context "when there is a latest_version" do
             before { td.create_version("1.2.107") }
+
+            it "has the options passed through correctly" do
+              allow_any_instance_of(EmbeddedVersionDecorator).to receive(:version_url) do | _instance, actual_base_url, _actual_version |
+                @actual_base_url = actual_base_url
+                "version_url"
+              end
+              subject
+              expect(@actual_base_url).to eq base_url
+            end
+
             it "includes an embedded latestVersion" do
               expect(subject[:_embedded][:latestVersion]).to include number: "1.2.107"
             end
