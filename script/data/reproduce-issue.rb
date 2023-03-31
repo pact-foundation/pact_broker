0ac80b9301807d06bea0f62ecd2171f71bf206f4
Beth Skurrie <beth.skurrie@smartbear.com>
2023-03-31 14:39:40 +1100

    chore: update reproduce-issue script [ci-skip]

--- script/data/reproduce-issue.rb
+++ script/data/reproduce-issue.rb
@@ -4,12 +4,6 @@
 # If you want to simulate publishing a pact that has the same content as a previous pact, use the same ID.
 # If you want to simulate publishing a pact with different content, use a different ID.
 
-#!/usr/bin/env ruby
-
-# The content_id is used to deterministically generate pact content. It doesn't matter what the value is.
-# If you want to simulate publishing a pact that has the same content as a previous pact, use the same ID.
-# If you want to simulate publishing a pact with different content, use a different ID.
-
 require "bundler/inline"
 
 gemfile do
@@ -23,13 +17,15 @@ begin
   require "pact_broker/test/http_test_data_builder"
   base_url = ENV["PACT_BROKER_BASE_URL"] || "http://localhost:9292"
 
+  CONSUMER = "foo"
+  PROVIDER = "bar"
+
   td = PactBroker::Test::HttpTestDataBuilder.new(base_url)
-  td.delete_pacticipant("foo-consumer-1")
-    .delete_pacticipant("foo-consumer-2")
-    .delete_pacticipant("bar-provider-1")
-    .delete_pacticipant("bar-provider-2")
-    .publish_pact_the_old_way(consumer: "foo-consumer-1", consumer_version: "1", provider: "bar-provider-1", content_id: "111", branch: "feat/x")
+  td.delete_pacticipant(CONSUMER)
+    .delete_pacticipant(PROVIDER)
+    .publish_contract(consumer: CONSUMER, consumer_version: "1", provider: PROVIDER, content_id: "111", branch: "main")
     .get_pacts_for_verification(
+      provider: PROVIDER,
       enable_pending: true,
       provider_version_branch: "main",
       include_wip_pacts_since: "2020-01-01",
@@ -37,38 +33,15 @@ begin
     )
     .verify_pact(
       index: 0,
+      provider: PROVIDER,
       provider_version_tag: "main",
       provider_version: "1",
       success: true
     )
-    .publish_pact_the_old_way(consumer: "foo-consumer-2", consumer_version: "1", provider: "bar-provider-1", content_id: "112", branch: "feat/y")
-    .get_pacts_for_verification(
-      enable_pending: true,
-      provider_version_branch: "main",
-      include_wip_pacts_since: "2020-01-01",
-      consumer_version_selectors: [{ branch: "main" }]
-    )
-    .verify_pact(
-      index: 0,
-      provider_version_tag: "main",
-      provider_version: "1",
-      success: true
-    )
-    .publish_pact_the_old_way(consumer: "foo-consumer-2", consumer_version: "2", provider: "bar-provider-2", content_id: "113", branch: "feat/z")
-    .get_pacts_for_verification(
-      enable_pending: true,
-      provider_version_branch: "main",
-      include_wip_pacts_since: "2020-01-01",
-      consumer_version_selectors: [{ branch: "main" }]
-    )
-    .verify_pact(
-      index: 0,
-      provider_version_tag: "main",
-      provider_version: "1",
-      success: true
-    )
-    .deploy_to_prod(pacticipant: "foo-consumer-2", version: "2")
-    .can_i_deploy(pacticipant: "bar-provider-1", version: "1", to: "prod")
+    .can_i_deploy(pacticipant: PROVIDER, version: "1", to_environment: "production")
+    .record_deployment(pacticipant: PROVIDER, version: "1", environment_name: "production")
+    .can_i_deploy(pacticipant: CONSUMER, version: "1", to_environment: "production")
+    .record_deployment(pacticipant: CONSUMER, version: "1", environment_name: "production")
 
 rescue StandardError => e
   puts "#{e.class} #{e.message}"
