913e0a52f8626359004e991c8867eab4e73b17d9
Beth Skurrie <beth.skurrie@smartbear.com>
2023-05-08 13:35:15 +1000

    fix: add extra validation to ensure parsed content is a hash when publishing pacts

--- lib/pact_broker/api/contracts/publish_contracts_contract_contract.rb
+++ lib/pact_broker/api/contracts/publish_contracts_contract_contract.rb
@@ -20,23 +20,7 @@ module PactBroker
         rule(:consumerName).validate(:not_blank_if_present)
         rule(:providerName).validate(:not_blank_if_present)
 
-        # validate_consumer_name_in_content
-        rule(:decodedParsedContent, :consumerName, :specification) do
-          consumer_name_in_content = values.dig(:decodedParsedContent, :consumer, :name)
-          if consumer_name_in_content && consumer_name_in_content != values[:consumerName]
-            base.failure(validation_message("consumer_name_in_content_mismatch", { consumer_name_in_content: consumer_name_in_content, consumer_name: values[:consumerName] }))
-          end
-        end
-
-        # validate_provider_name_in_content
-        rule(:decodedParsedContent, :providerName) do
-          provider_name_in_content = values.dig(:decodedParsedContent, :provider, :name)
-          if provider_name_in_content && provider_name_in_content != values[:providerName]
-            base.failure(validation_message("provider_name_in_content_mismatch", { provider_name_in_content: provider_name_in_content, provider_name: values[:providerName] }))
-          end
-        end
-
-        # validate_encoding
+        # validate_encoding (ensure all UTF-8 chars)
         rule(:decodedContent) do
           if value.nil?
             base.failure(validation_message("base64"))
@@ -50,10 +34,37 @@ module PactBroker
           end
         end
 
-        # validate_content_matches_content_type
+        # validate content could be parsed according to its content type
         rule(:decodedParsedContent, :contentType) do
           if values[:decodedParsedContent].nil? && values[:contentType]
             base.failure(validation_message("invalid_content_for_content_type", { content_type: values[:contentType] }))
+          end
+        end
+
+        # validate parsed contract is a hash
+        rule(:decodedParsedContent, :contentType) do
+          if !base_rule_error? && !values[:decodedParsedContent].is_a?(Hash)
+            base.failure(validation_message("invalid_parsed_contract_class", { actual_class: values[:decodedParsedContent].class }))
+          end
+        end
+
+        # validate consumer name in content matches the details higher in the JSON document
+        rule(:decodedParsedContent, :consumerName, :specification) do
+          if !base_rule_error?
+            consumer_name_in_content = values.dig(:decodedParsedContent, :consumer, :name)
+            if consumer_name_in_content && consumer_name_in_content != values[:consumerName]
+              base.failure(validation_message("consumer_name_in_content_mismatch", { consumer_name_in_content: consumer_name_in_content, consumer_name: values[:consumerName] }))
+            end
+          end
+        end
+
+        # validate provider name in content matches the details higher in the JSON document
+        rule(:decodedParsedContent, :providerName) do
+          if !base_rule_error?
+            provider_name_in_content = values.dig(:decodedParsedContent, :provider, :name)
+            if provider_name_in_content && provider_name_in_content != values[:providerName]
+              base.failure(validation_message("provider_name_in_content_mismatch", { provider_name_in_content: provider_name_in_content, provider_name: values[:providerName] }))
+            end
           end
         end
       end
