bf5c8c263669ba1ca9567073c342aa778a7498c6
Beth Skurrie <bethesque@users.noreply.github.com>
2023-06-19 12:03:34 +1000

    chore: add schema for pagination query parameters (#620)
    PACT-1070

---
+++ spec/lib/pact_broker/api/contracts/pagination_query_params_schema_spec.rb
@@ -0,0 +1,43 @@
+require "pact_broker/api/contracts/pagination_query_params_schema"
+
+module PactBroker
+  module Api
+    module Contracts
+      describe PaginationQueryParamsSchema do
+        let(:params) do
+          {}
+        end
+
+        subject { PaginationQueryParamsSchema.call(params) }
+
+        context "with empty params" do
+          it { is_expected.to be_empty }
+        end
+
+        context "with values that are not numeric" do
+          let(:params) do
+            {
+              "pageNumber" => "a",
+              "pageSize" => "3.2"
+            }
+          end
+
+          its([:pageNumber]) { is_expected.to contain_exactly(match("integer"))}
+          its([:pageSize]) { is_expected.to contain_exactly(match("integer"))}
+        end
+
+        context "with values that are 0" do
+          let(:params) do
+            {
+              "pageNumber" => "-0",
+              "pageSize" => "-0"
+            }
+          end
+
+          its([:pageNumber]) { is_expected.to contain_exactly(match(/greater.*1/))}
+          its([:pageSize]) { is_expected.to contain_exactly(match(/greater.*1/))}
+        end
+      end
+    end
+  end
+end
