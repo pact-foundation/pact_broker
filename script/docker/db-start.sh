cd2832c8b3fde221384d2aca9b2ee59c67e6c6dd
Beth Skurrie <beth.skurrie@smartbear.com>
2023-09-04 13:27:22 +1000

    chore: update db-start

--- script/docker/db-start.sh
+++ script/docker/db-start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Add these arguments to deliberately slow down the database when working on performance issues
+#
+# --cpu-period=100000 --cpu-quota=50000 \
+#
 docker run --name pact-broker-postgres \
   -e POSTGRES_PASSWORD=postgres \
   -p 5432:5432 \
   -v $PWD:/data \
-  -d postgres:12
+  -d postgres:14
