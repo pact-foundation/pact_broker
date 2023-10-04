475ccd838bcd6a39177fb15558877d1703213977
Beth Skurrie <beth.skurrie@smartbear.com>
2023-10-04 14:36:24 +1100

    chore: do not add indexes when mysql

--- db/migrations/20231003_add_version_id_index_to_deployed_version.rb
+++ db/migrations/20231003_add_version_id_index_to_deployed_version.rb
@@ -1,7 +1,13 @@
+require_relative "migration_helper"
+
+include PactBroker::MigrationHelper
+
 Sequel.migration do
   change do
-    alter_table(:deployed_versions) do
-      add_index(:version_id, name: "deployed_versions_version_id_ndx")
+    if !mysql?
+      alter_table(:deployed_versions) do
+        add_index(:version_id, name: "deployed_versions_version_id_ndx")
+      end
     end
   end
 end
