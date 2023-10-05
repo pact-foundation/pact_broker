2a01caebd1a3a5ef79d84d954117a2f324244fe6
Beth Skurrie <beth.skurrie@smartbear.com>
2023-10-05 13:09:37 +1100

    chore: split index migrations into up and down

--- db/migrations/20231003_add_version_id_index_to_deployed_version.rb
+++ db/migrations/20231003_add_version_id_index_to_deployed_version.rb
@@ -3,10 +3,18 @@ require_relative "migration_helper"
 include PactBroker::MigrationHelper
 
 Sequel.migration do
-  change do
+  up do
     if !mysql?
       alter_table(:deployed_versions) do
         add_index(:version_id, name: "deployed_versions_version_id_ndx")
+      end
+    end
+  end
+
+  down do
+    if !mysql?
+      alter_table(:deployed_versions) do
+        drop_index(:version_id, name: "deployed_versions_version_id_ndx")
       end
     end
   end
