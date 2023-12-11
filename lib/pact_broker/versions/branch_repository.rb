d53204c8888c98a258e6e59b2f4878ca88faab3b
Beth Skurrie <bethesque@users.noreply.github.com>
2023-12-11 12:05:28 +1100

    feat: bulk delete branches (#652)

--- lib/pact_broker/versions/branch_repository.rb
+++ lib/pact_broker/versions/branch_repository.rb
@@ -38,6 +38,39 @@ module PactBroker
       def delete_branch(branch)
         branch.delete
       end
+
+      # @param [PactBroker::Domain::Pacticipant] pacticipant
+      # @params [Array<String>] exclude the names of the branches to NOT delete
+      # @param [Integer] the number of branches that will be deleted
+      def count_branches_to_delete(pacticipant, exclude: )
+        build_query_for_pacticipant_branches(pacticipant, exclude: exclude).count
+      end
+
+      # Returns the list of branches which will NOT be deleted (the bulk delete is executed async after the request has finished)
+      # @param [PactBroker::Domain::Pacticipant] pacticipant
+      # @params [Array<String>] exclude the names of the branches to NOT delete
+      # @return [Array<PactBroker::Versions::Branch>]
+      def remaining_branches_after_future_deletion(pacticipant, exclude: )
+        exclude_dup = exclude.dup
+        if pacticipant.main_branch
+          exclude_dup << pacticipant.main_branch
+        end
+        Branch.where(pacticipant_id: pacticipant.id).where(name: exclude_dup)
+      end
+
+      # @param [PactBroker::Domain::Pacticipant] pacticipant
+      # @params [Array<String>] exclude the names of the branches to NOT delete
+      def delete_branches_for_pacticipant(pacticipant, exclude:)
+        build_query_for_pacticipant_branches(pacticipant, exclude: exclude).delete
+      end
+
+      def build_query_for_pacticipant_branches(pacticipant, exclude: )
+        exclude_dup = exclude.dup
+        if pacticipant.main_branch
+          exclude_dup << pacticipant.main_branch
+        end
+        Branch.where(pacticipant_id: pacticipant.id).exclude(name: exclude_dup)
+      end
     end
   end
 end
