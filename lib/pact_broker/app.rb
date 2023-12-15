e2b2fcf2feb848e6e1ad9931032e75ea08790a93
Beth Skurrie <beth.skurrie@smartbear.com>
2023-12-15 13:55:17 +1100

    chore: move no-cache header after static assets middleware

--- lib/pact_broker/app.rb
+++ lib/pact_broker/app.rb
@@ -185,10 +185,10 @@ module PactBroker
       @app_builder.use Rack::PactBroker::InvalidUriProtection
       @app_builder.use Rack::PactBroker::ResetThreadData
       @app_builder.use Rack::PactBroker::AddPactBrokerVersionHeader
-      @app_builder.use Rack::PactBroker::AddCacheHeader
       @app_builder.use Rack::PactBroker::AddVaryHeader
       @app_builder.use Rack::Static, :urls => ["/stylesheets", "/css", "/fonts", "/js", "/javascripts", "/images"], :root => PactBroker.project_root.join("public")
       @app_builder.use Rack::Static, :urls => ["/favicon.ico"], :root => PactBroker.project_root.join("public/images"), header_rules: [[:all, {"Content-Type" => "image/x-icon"}]]
+      @app_builder.use Rack::PactBroker::AddCacheHeader
       @app_builder.use Rack::PactBroker::ConvertFileExtensionToAcceptHeader
       # Rack::PactBroker::SetBaseUrl needs to be before the Rack::PactBroker::HalBrowserRedirect
       @app_builder.use Rack::PactBroker::SetBaseUrl, configuration.base_urls
