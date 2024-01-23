5d9d70022f9e6f86c3747f7a2662e1498176319c
Beth Skurrie <bethesque@users.noreply.github.com>
2024-01-23 14:03:00 +1100

    feat: use SemanticLogger for Padrino (#662)

--- lib/pact_broker/ui.rb
+++ lib/pact_broker/ui.rb
@@ -1,14 +1,25 @@
-require "pact_broker/configuration"
+# Must be defined before loading Padrino
 # Stop Padrino creating a log file, as it will try to create it in the gems directory
 # http://www.padrinorb.com/api/Padrino/Logger.html
-unless defined? PADRINO_LOGGER
-  log_path = File.join(PactBroker.configuration.log_dir, "ui.log")
-  PADRINO_LOGGER = {
-    production:  { log_level: :error, stream: :to_file, log_path: log_path },
-    staging:     { log_level: :error, stream: :to_file, log_path: log_path },
-    test:        { log_level: :warn,  stream: :to_file, log_path: log_path },
-    development: { log_level: :warn,  stream: :to_file, log_path: log_path }
-  }
+# This configuration will be replaced by the SemanticLogger later on.
+PADRINO_LOGGER ||= {
+  ENV.fetch("RACK_ENV", "production").to_sym =>  { stream: :stdout }
+}
+
+require "padrino-core"
+
+class PactBrokerPadrinoLogger < SemanticLogger::Logger
+  include Padrino::Logger::Extensions
+
+  # Padrino expects level to return an integer, not a symbol
+  def level
+    Padrino::Logger::Levels[SemanticLogger.default_level]
+  end
 end
+
+Padrino.logger = PactBrokerPadrinoLogger.new("Padrino")
+# Log a test message to ensure that the logger works properly, as it only
+# seems to be used in production.
+Padrino.logger.info("Padrino has been configured with SemanticLogger")
 
 require "pact_broker/ui/app"
