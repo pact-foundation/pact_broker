0220040e93aef146aa5bd467cdcb164a42e28a6a
Beth Skurrie <beth@bethesque.com>
2024-03-25 11:36:19 +1100

    feat: add lock to clean task

--- lib/pact_broker/tasks/clean_task.rb
+++ lib/pact_broker/tasks/clean_task.rb
@@ -1,3 +1,8 @@
+# This task is used to clean up old data in a Pact Broker database
+# to stop performance issues from slowing down responses when there is
+# too much data.
+# See https://docs.pact.io/pact_broker/administration/maintenance
+
 module PactBroker
   module DB
     class CleanTask < ::Rake::TaskLib
@@ -7,11 +12,13 @@ module PactBroker
       attr_accessor :version_deletion_limit
       attr_accessor :logger
       attr_accessor :dry_run
+      attr_accessor :use_lock # allow disabling of postgres lock if it is causing problems
 
       def initialize &block
         require "pact_broker/db/clean_incremental"
         @version_deletion_limit = 1000
         @dry_run = false
+        @use_lock = true
         @keep_version_selectors = PactBroker::DB::CleanIncremental::DEFAULT_KEEP_SELECTORS
         rake_task(&block)
       end
@@ -28,42 +35,76 @@ module PactBroker
           namespace :db do
             desc "Clean unnecessary pacts and verifications from database"
             task :clean do | _t, _args |
-
               instance_eval(&block)
 
-              require "pact_broker/db/clean_incremental"
-              require "pact_broker/error"
-              require "yaml"
-              require "benchmark"
-
-              raise PactBroker::Error.new("You must specify the version_deletion_limit") unless version_deletion_limit
-
-              prefix = dry_run ? "[DRY RUN] " : ""
-
-              if keep_version_selectors.nil? || keep_version_selectors.empty?
-                raise PactBroker::Error.new("You must specify which versions to keep")
-              else
-                add_defaults_to_keep_selectors
-                output "#{prefix}Deleting oldest #{version_deletion_limit} versions, keeping versions that match the configured selectors", keep_version_selectors.collect(&:to_hash)
+              with_lock do
+                perform_clean
               end
-
-              start_time = Time.now
-              results = PactBroker::DB::CleanIncremental.call(database_connection,
-                keep: keep_version_selectors,
-                limit: version_deletion_limit,
-                logger: logger,
-                dry_run: dry_run
-              )
-              end_time = Time.now
-              elapsed_seconds = (end_time - start_time).to_i
-              output "Results (#{elapsed_seconds} seconds)", results
             end
           end
         end
       end
 
-      def output string, payload = {}
-        logger ? logger.info(string, payload) : puts("#{string} #{payload.to_json}")
+      def perform_clean
+        require "pact_broker/db/clean_incremental"
+        require "pact_broker/error"
+        require "yaml"
+        require "benchmark"
+
+        raise PactBroker::Error.new("You must specify the version_deletion_limit") unless version_deletion_limit
+
+        if keep_version_selectors.nil? || keep_version_selectors.empty?
+          raise PactBroker::Error.new("You must specify which versions to keep")
+        else
+          add_defaults_to_keep_selectors
+          output "Deleting oldest #{version_deletion_limit} versions, keeping versions that match the configured selectors", keep_version_selectors.collect(&:to_hash)
+        end
+
+        start_time = Time.now
+        results = PactBroker::DB::CleanIncremental.call(database_connection,
+          keep: keep_version_selectors,
+          limit: version_deletion_limit,
+          logger: logger,
+          dry_run: dry_run
+        )
+        end_time = Time.now
+        elapsed_seconds = (end_time - start_time).to_i
+        output "Results (#{elapsed_seconds} seconds)", results
+      end
+
+      # Use a Postgres advisory lock to ensure that only one clean can run at a time.
+      # This allows a cron schedule to be used on the Pact Broker Docker image when deployed
+      # on a multi-instance architecture, without all the instances stepping on each other's toes.
+      #
+      # Any tasks that attempt to run while a clean job is running will skip the clean
+      # and exit with a message and a success code.
+      #
+      # To test that the lock works, run:
+      #   script/docker/db-start.sh
+      #   script/docker/db-migrate.sh
+      #   for i in {0..3}; do PACT_BROKER_TEST_DATABASE_URL=postgres://postgres:postgres@localhost/postgres bundle exec rake pact_broker:db:clean &;  done;
+      #
+      # There will be 3 messages saying "Clean was not performed" and output from one thread showing the clean is being done.
+      def with_lock
+        if use_lock
+          require "sequel/postgres_advisory_lock"
+
+          lock = Sequel::PostgresAdvisoryLock.new(database_connection, :clean, :pg_try_advisory_lock)
+          results = lock.with_lock do
+            yield
+          end
+
+          if !lock.lock_obtained?
+            output("Clean was not performed as a clean is already in progress. Exiting.")
+          end
+        else
+          yield
+        end
+      end
+
+      def output(string, payload = {})
+        prefix = dry_run ? "[DRY RUN] " : ""
+        logger ? logger.info("#{prefix}#{string}") : puts("#{prefix}#{string} #{payload.to_json}")
       end
 
       def add_defaults_to_keep_selectors
--- lib/sequel/postgres_advisory_lock.rb
+++ lib/sequel/postgres_advisory_lock.rb
@@ -1,25 +1,41 @@
+require "pact_broker/logging"
+
+# Docs on lock types from From https://www.postgresql.org/docs/9.1/functions-admin.html
+#
+# pg_advisory_lock locks an application-defined resource.
+# If another session already holds a lock on the same resource identifier, this function will wait until the
+# resource becomes available.
+# The lock is exclusive.
+# Multiple lock requests stack, so that if the same resource is locked three times it must then be unlocked three
+# times to be released for other sessions' use.
+
+# pg_advisory_lock_shared works the same as pg_advisory_lock, except the lock can be shared with other sessions
+# requesting shared locks. Only would-be exclusive lockers are locked out.
+
+# pg_try_advisory_lock is similar to pg_advisory_lock, except the function will not wait for the lock to become available.
+# It will either obtain the lock immediately and return true, or return false if the lock cannot be acquired immediately.
+
 module Sequel
   class PostgresAdvisoryLock
+    include PactBroker::Logging
+
     def initialize(database_connection, name, type = :pg_try_advisory_lock)
       @database_connection = database_connection
       @name = name
       @type = type
       @lock_obtained = false
-      if postgres?
-        @database_connection.extension :pg_advisory_lock
-        unless @database_connection.registered_advisory_locks.key?(@name)
-          @database_connection.register_advisory_lock(@name, @type)
-        end
-      end
+      register_advisory_lock if postgres?
     end
 
     def with_lock
       if postgres?
         @database_connection.with_advisory_lock(@name) do
+          logger.debug("Lock #{@name} obtained")
           @lock_obtained = true
           yield
         end
       else
+        logger.debug("Executing without lock as this is not a postgres database")
         @lock_obtained = true
         yield
       end
@@ -29,8 +45,23 @@ module Sequel
       @lock_obtained
     end
 
+    private
+
     def postgres?
       @database_connection.adapter_scheme.to_s =~ /postgres/
+    end
+
+
+    def register_advisory_lock
+      @database_connection.extension :pg_advisory_lock
+      unless @database_connection.registered_advisory_locks.key?(@name)
+        logger.debug("Registering postgres lock of type #{@type} with name #{@name}")
+        begin
+          @database_connection.register_advisory_lock(@name, @type)
+        rescue Sequel::Error => e
+          logger.info(e.message)
+        end
+      end
     end
   end
 end
