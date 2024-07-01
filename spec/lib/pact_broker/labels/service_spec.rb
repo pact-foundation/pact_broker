038c3c51c46658924ab2b967098460f30e4eeef7
Sauparna Gupta <sauparna.gupta@gmail.com>
2024-07-01 11:16:54 +0530

    chore: added test for service layer of Label service

--- spec/lib/pact_broker/labels/service_spec.rb
+++ spec/lib/pact_broker/labels/service_spec.rb
@@ -5,7 +5,17 @@ module PactBroker
     describe Service do
       let(:pacticipant_name) { "foo" }
       let(:label_name) { "ios" }
-      let(:options) { {pacticipant_name: pacticipant_name, label_name: label_name}}
+      let(:options) { {pacticipant_name: pacticipant_name, label_name: label_name } }
+      let(:pagination_options) { { page_number: 1, page_size: 1 } }
+
+      describe ".get_all_unique_labels" do
+        subject { Service.get_all_unique_labels(pagination_options) }
+
+        it "calls the labels repository" do
+          expect_any_instance_of(Labels::Repository).to receive(:get_all_unique_labels).with(pagination_options)
+          subject
+        end
+      end
 
       describe ".create" do
         subject { Service.create(options) }
