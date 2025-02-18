a34e865f3b7e73e45b9eeec2379b1a9df1f12789
Andreas Haller <ahx@posteo.de>
2025-02-18 19:37:33 +0100

    Add test about responding with application/problem+json
    Fix API coverage issue. This results in removal of this test output:
    
    ✓ POST /contracts/publish (application/json)
      ✓  200(application/hal+json)
      ❌ 400(application/problem+json) – No responses tracked!
      ✓  400(application/hal+json)
      ✓  409(application/json)
    The overal API validation coverage of this run is: 80%
    Exiting with status 2 (failure), because API coverage was 80% instead of 100%!

--- spec/features/publish_pact_all_in_one_spec.rb
+++ spec/features/publish_pact_all_in_one_spec.rb
@@ -46,6 +46,14 @@ RSpec.describe "publishing a pact using the all in one endpoint", validate_oas: 
     let(:contract) { "{\"key\": \"ABCDEFG\x8FDEF\" }" }
 
     its(:status) { is_expected.to eq 400 }
+    its(:media_type) { is_expected.to eq "application/hal+json" }
+
+    context "when client accepts application/problem+json" do
+      let(:rack_headers) { { "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT" => "application/problem+json" } }
+
+      its(:status) { is_expected.to eq 400 }
+      its(:media_type) { is_expected.to eq "application/problem+json" }
+    end
   end
 
   context "with a conflicting pact" do
