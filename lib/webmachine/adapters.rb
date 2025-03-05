20c3f9f44065c5ea04d3b7b0b25eefbc0da5a80d
Yousaf Nabi <yousaf@pactflow.io>
2025-03-05 16:22:28 +0000

    fix: add webmachine monkey patch to exclude webrick default load

---
+++ lib/webmachine/adapters.rb
@@ -0,0 +1,9 @@
+require "webmachine/adapters/lazy_request_body"
+# require "webmachine/adapters/webrick" # Commenting out this line as our monkey patch
+
+module Webmachine
+  # Contains classes and modules that connect Webmachine to Ruby
+  # application servers.
+  module Adapters
+  end
+end
