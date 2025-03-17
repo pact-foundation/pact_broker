79dbe1fc4102e5f59bb730bff356adcb1cd9d95d
Yousaf Nabi <yousaf@pactflow.io>
2025-03-17 19:52:51 +0000

    test: add failing spec to demonstrate issue #789

--- spec/features/list_provider_states_spec.rb
+++ spec/features/list_provider_states_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "listing the provider states" do
+RSpec.describe "listing the provider states without params" do
   before do
     td.create_consumer("Foo", main_branch: "main")
       .publish_pact(consumer_name: "Foo", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
@@ -35,10 +35,95 @@ RSpec.describe "listing the provider states" do
     }
   end
 
+  let(:expected_hash) do
+    {
+      "providerStates" => [ 
+        { "name" => "state 1" }, 
+        { "name" => "state 2" }, 
+        { "name" => "state 3" }, 
+        { "name" => "state 4" }, 
+        { "name" => "state 5" } 
+      ]
+    }
+  end
+
+  let(:response_body_hash) { JSON.parse(subject.body) }
+
   let(:path) { "/pacts/provider/Bar/provider-states" }
 
   subject { get(path, nil, rack_headers).tap { |it| puts it.body } }
 
-  it { is_expected.to be_a_hal_json_success_response }
+  it { 
+    is_expected.to be_a_hal_json_success_response
+    expect(response_body_hash).to eq expected_hash
+  }
+
+end
+
+RSpec.describe "listing the provider states with params" do
+  before do
+    td.create_consumer("Foo", main_branch: "main")
+      .publish_pact(consumer_name: "Foo", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
+      .publish_pact(consumer_name: "Foo2", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
+      .publish_pact(consumer_name: "Foo3", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
+      .publish_pact(consumer_name: "Foo4", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
+      .publish_pact(consumer_name: "Foo5", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_1.to_json)
+      .publish_pact(consumer_name: "Foo6", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_3.to_json)
+      .publish_pact(consumer_name: "Foo", provider_name: "Bar", consumer_version_number: "2", branch: "not-main").create_consumer("Waffle", main_branch: "main")
+      .publish_pact(consumer_name: "Waffle", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_2.to_json)
+      .publish_pact(consumer_name: "Waffle2", provider_name: "Bar", consumer_version_number: "1", branch: "main", json_content: pact_content_2.to_json)
+  end
+
+  let(:rack_headers) { { "HTTP_ACCEPT" => "application/hal+json" } }
+
+  let(:pact_content_1) do
+    {
+      interactions: [
+        {
+          providerStates: [ { name: "product details", params: { product_id: "058925f7-1763-4dd9-a057-50ee265e33a0" } } ]
+        },
+      ]
+    }
+  end
+
+  let(:pact_content_2) do
+    {
+      interactions: [
+        {
+          providerStates: [ { name: "product list" } ]
+        }
+      ]
+    }
+  end
+
+  let(:pact_content_3) do
+    {
+      interactions: [
+        {
+          providerStates: [ { name: "some other product list" } ]
+        }
+      ]
+    }
+  end
+
+  let(:expected_hash) do
+    {
+      "providerStates" => [ 
+        { "name" => "product details", "params"=>{"product_id"=>"058925f7-1763-4dd9-a057-50ee265e33a0"}  }, 
+        { "name" => "product list" },
+      ]
+    }
+  end
+
+  let(:response_body_hash) { JSON.parse(subject.body) }
+
+  let(:path) { "/pacts/provider/Bar/provider-states" }
+
+  subject { get(path, nil, rack_headers).tap { |it| puts it.body } }
+
+  it { 
+    is_expected.to be_a_hal_json_success_response
+    expect(response_body_hash).to eq expected_hash
+  }
 
 end
