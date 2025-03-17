e39860a902fbbc4b31d9bdf0ef3927dbfd28bae5
Yousaf Nabi <yousaf@pactflow.io>
2025-03-17 19:56:20 +0000

    feat: group provider states by consumers
    ```
        {
          "providerStates" => [
            { "consumers" => ["Foo", "Foo2", "Foo3", "Foo4", "Foo5"], "name" => "product details", "params" => { "product_id" => "058925f7-1763-4dd9-a057-50ee265e33a0" } },
            { "consumers" => ["Waffle", "Waffle2"], "name" => "product list" },
            { "consumers" => ["Foo6"], "name" => "some other product list" }
          ]
        }
    ```

--- lib/pact_broker/pacts/provider_state_service.rb
+++ lib/pact_broker/pacts/provider_state_service.rb
@@ -14,8 +14,8 @@ module PactBroker
       def self.list_provider_states(provider)
         query = scope_for(PactPublication).eager_for_domain_with_content.for_provider_and_consumer_version_selector(provider, PactBroker::Pacts::Selector.latest_for_main_branch)
         query.all.flat_map do | pact_publication |
-          pact_publication.to_domain.content_object.provider_states
-        end.uniq
+          { "providerStates" => pact_publication.to_domain.content_object.provider_states, "consumer" => pact_publication.to_domain.consumer.name }
+        end
       end
     end
   end
