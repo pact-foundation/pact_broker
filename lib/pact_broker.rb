2af1a23f9d3488ecc5b515324ad458c49135cec6
Sauparna Gupta <sauparna.gupta@gmail.com>
2026-01-17 03:42:25 +0530

    chore: wip/refactoring pactbroker config

--- lib/pact_broker.rb
+++ lib/pact_broker.rb
@@ -2,3 +2,19 @@ require "pact_broker/version"
 require "pact_broker/logging"
 require "pact_broker/app"
 require "pact_broker/db/log_quietener"
+require "request_store"
+require "pact_broker/configuration"
+module PactBroker
+  def self.configuration
+    RequestStore.store[:pact_broker_configuration] ||= Configuration.default_configuration
+  end
+
+  def self.set_configuration(configuration)
+    RequestStore.store[:pact_broker_configuration] = configuration
+  end
+
+  # @private, for testing only
+  def self.reset_configuration
+    RequestStore.store[:pact_broker_configuration] = Configuration.default_configuration
+  end
+end
