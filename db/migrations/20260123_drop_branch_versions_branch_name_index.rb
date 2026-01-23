3c615179b5baccdbd22c493dccea9472a45f82b7
Steven Holloway <impurist@gmail.com>
2026-01-23 11:26:50 +1100

    chore: fix migration syntax.

--- db/migrations/20260123_drop_branch_versions_branch_name_index.rb
+++ db/migrations/20260123_drop_branch_versions_branch_name_index.rb
@@ -1,13 +1,17 @@
 Sequel.migration do
   up do
-    alter_table(:branch_versions) do
-      drop_index :branch_versions_branch_name_index
+    if !mysql?
+      alter_table(:branch_versions) do
+        drop_index([:branch_name], name: "branch_versions_branch_name_index")
+      end
     end
   end
 
   down do
-    alter_table(:branch_versions_branch_name_index) do
-      add_index :branch_versions_branch_name_index, :branch_name
+    if !mysql?
+      alter_table(:branch_versions) do
+        add_index([:branch_name], name: "branch_versions_branch_name_index")
+      end
     end
   end
 end
