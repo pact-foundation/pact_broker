ecc90eb65770255643d2986694fa769802ba53d5
Tuan Pham <tuan.pham@smartbear.com>
2026-02-10 11:04:59 +1100

    chore(PACT-5504): add index to improve query time

---
+++ db/migrations/20260210_add_index_on_pact_version_id_to_latest_pact_publication.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  up do
+    if !mysql?
+      alter_table(:latest_pact_publication_ids_for_consumer_versions) do
+        add_index :pact_version_id, name: :latest_pp_ids_for_cons_ver_pact_version_id_index
+      end
+    end
+  end
+
+  down do
+    if !mysql?
+      alter_table(:latest_pact_publication_ids_for_consumer_versions) do
+        drop_index :pact_version_id, name: :latest_pp_ids_for_cons_ver_pact_version_id_index
+      end
+    end
+  end
+end
+
