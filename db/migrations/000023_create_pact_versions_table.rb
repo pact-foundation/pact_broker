7981300b8db4bae51235d41e3958a22a9d799824
Steven Holloway <impurist@gmail.com>
2026-07-08 08:56:35 +1000

    chore: fix column type mismatch warnings for both postgres and mysql.

--- db/migrations/000023_create_pact_versions_table.rb
+++ db/migrations/000023_create_pact_versions_table.rb
@@ -7,7 +7,7 @@ Sequel.migration do
       foreign_key :consumer_id, :pacticipants
       foreign_key :provider_id, :pacticipants
       String :sha, null: false
-      String :content, type: PactBroker::MigrationHelper.large_text_type
+      column :content, PactBroker::MigrationHelper.large_text_type
       index [:consumer_id, :provider_id, :sha], unique: true, name: "unq_pvc_con_prov_sha"
       DateTime :created_at, null: false
     end
--- db/migrations/20180108_create_certificates_table.rb
+++ db/migrations/20180108_create_certificates_table.rb
@@ -6,7 +6,7 @@ Sequel.migration do
       primary_key :id
       String :uuid, null: false, unique: true, unique_constraint_name: "uq_certificate_uuid"
       String :description, null: true
-      String :content, null: false, type: PactBroker::MigrationHelper.large_text_type
+      column :content, PactBroker::MigrationHelper.large_text_type, null: false
       DateTime :created_at, null: false
       DateTime :updated_at, null: false
     end
